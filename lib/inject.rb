62fffd74c7641957b911b25b18e75bca927d718b
Yannick Brunner <yannickbrunner@gmail.com>
2015-02-13 15:11:12 +0000

    add the block aspect of the inject method and merged it with the symbol one, needs a lot of refactoring

--- lib/inject.rb
+++ lib/inject.rb
@@ -5,51 +5,40 @@ class Array
 
   # attr_reader :copy, :result
 
-  # def new_inect(*args, &block)
-
-  #   block_given?
-  #   @arr = self.dup
-    
-
-
-  #   result
-
-  # end
-
-  # def block_inject(*args)
-  #   yield(result, element)
-  # end
-
-
-
-
-  def symbol_inject(*args)
+   def new_inject(*args)
 
     @arr = self.dup
 
-    if args.length == 2
-      @symbol = args[1]
-      @result = args[0]
-    else
-      @symbol = args[0]
-      @result = @arr[0]
-      @arr.shift
-    end
+      if block_given?  
 
-    @arr.each {|item| @result = @result.send @symbol, item}
+        if args.empty?
+          @result = @arr[0]
+          @arr.shift
+        else
+          @result = args[0]
+        end
+
+        @arr.each do |element|
+          @result = yield(@result, element)
+        end
+
+      else
+      
+        if args.length == 2
+          @symbol = args[1]
+          @result = args[0]
+        else
+          @symbol = args[0]
+          @result = @arr[0]
+          @arr.shift
+        end
+
+      @arr.each {|item| @result = @result.send @symbol, item}
+
+    end
+    
     @result
 
-  end
-
-
-
-  # # check if the method is receive a symbol or nothing
-  # def symbol_in_args?(args)
-  #   args[0].is_a?(Symbol)
-  # end
-
-  # def empty_args?(args)
-  #   args.empty?
-  # end
-
-end
+   end
+ end
+     
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -7,22 +7,29 @@ describe Array do
 
     it 'returns the sum of some numbers starting with the first number in the array' do
 
-      expect((5..10).inject {|sum, n| sum + n } ).to eq(45) 
-      #expect((5..10).new_inject {|sum, n| sum + n } ).to eq(45) 
+      expect(test.inject {|sum, n| sum + n } ).to eq(10) 
+      expect(test.new_inject {|sum, n| sum + n } ).to eq(10) 
 
     end
 
-    it 'returns the sum of some numbers starting with the first number given' do
+    it 'returns the sum of some numbers with the first number given' do
 
-      expect((5..10).inject(1) {|sum, n| sum + n } ).to eq(46) 
-      #expect((5..10).new_inject {|sum, n| sum + n } ).to eq(45) 
+      expect(test.inject(1) {|sum, n| sum + n } ).to eq(11) 
+      expect(test.new_inject(1) {|sum, n| sum + n } ).to eq(11) 
 
     end    
 
     it 'returns the product of some numbers' do
 
-      expect((5..10).inject {|product, n| product * n } ).to eq(151200) 
-      #expect((5..10).new_inject {|product, n| product * n } ).to eq(151200) 
+      expect(test.inject {|product, n| product * n } ).to eq(24) 
+      expect(test.new_inject {|product, n| product * n } ).to eq(24) 
+
+    end
+
+    it 'returns the product of some numbers with the first number given' do
+
+      expect(test.inject(2) {|product, n| product * n } ).to eq(48) 
+      expect(test.new_inject(2) {|product, n| product * n } ).to eq(48) 
 
     end
   end
@@ -30,27 +37,30 @@ describe Array do
   context 'with a method' do
 
     it 'can sum numbers using a symbol' do
-      expect([1,2,3,4].inject(:+)).to eq(10) 
-      expect([1,2,3,4].symbol_inject(:+)).to eq(10)
+      expect(test.inject(:+)).to eq(10) 
+      expect(test.new_inject(:+)).to eq(10)
     end
 
     it 'can sum numbers using a symbol and argument' do
-      expect([1,2,3,4].inject(3, :+)).to eq(13) 
-      expect([1,2,3,4].symbol_inject(3, :+)).to eq(13)
+      expect(test.inject(3, :+)).to eq(13) 
+      expect(test.new_inject(3, :+)).to eq(13)
     end
 
     it 'can multiply numbers using a symbol' do
-      expect([1,2,3,4].inject(:*)).to eq(24) 
-      expect([1,2,3,4].symbol_inject(:*)).to eq(24) 
+      expect(test.inject(:*)).to eq(24) 
+      expect(test.new_inject(:*)).to eq(24) 
     end
 
     it 'can multiply numbers using a symbol and an argument' do
-      expect([1,2,3,4].inject(1, :*)).to eq(24) 
-      expect([1,2,3,4].symbol_inject(1, :*)).to eq(24) 
+      expect(test.inject(1, :*)).to eq(24) 
+      expect(test.new_inject(1, :*)).to eq(24) 
 
 
     end
   end
 
+  def test
+    @test = [1,2,3,4]
+  end
 
 end
