713bd4a7103da09499edf171539f10d8354ffe7d
Chris Ward <chriswarddev@gmail.com>
2015-02-13 18:43:59 +0000

    All tests pass and replicates method

--- lib/array.rb
+++ lib/array.rb
@@ -1,7 +1,11 @@
 class Array
 
-	def my_inject(arg1)
-		6
+	def my_inject(arg)
+		return self.first if self.empty?
+		self.each do |item|
+			arg = yield arg , item
+		end
+		arg
 	end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,14 +1,25 @@
 require 'array'
 
 describe Array do 
-
-
-
-it 'should be able to sum an array of 3 numbers' do
-	array = [1,2,3]
-expect(array.my_inject(0){|start,number|start+number}).to eq (6)
-end
-
-
 	
+	it 'should be able to sum an array of 1,2,3 with the answer 6' do
+		array = [1,2,3]
+		expect(array.my_inject(0){|start,number|start+number}).to eq (6)
+	end
+
+	it 'should be able to sum an array of 2,3,4 with the answer 9' do
+		array = [2,3,4]
+		expect(array.my_inject(0){|start,number|start+number}).to eq (9)
+	end
+
+	it 'should be able to sum the squares of an array 1,2,3 with the answer 14' do
+		array = [1,2,3]
+		expect(array.my_inject(0){|start,number|start+number**2}).to eq (14)
+	end
+
+	it "should return nil if an empty array is passed" do
+		array = []
+		expect(array.my_inject(0){|start,number|start+number}).to eq nil
+	end
+
 end
