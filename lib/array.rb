a05f721e624fb99e6f97696f23c421ef39ecb17c
Chris Ward <chriswarddev@gmail.com>
2015-02-14 08:20:30 +0000

    method now has default argument value and test passes

--- lib/array.rb
+++ lib/array.rb
@@ -1,11 +1,14 @@
 class Array
 
-	def my_inject(arg)
-		return self.first if self.empty?
+	def my_inject(arg=0)
 		self.each do |item|
 			arg = yield arg , item
 		end
 		arg
+	end
+
+	def my_inject_input_array_is_empty
+		return self.first if self.empty?
 	end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,21 +5,33 @@ describe Array do
 	it 'should be able to sum an array of 1,2,3 with the answer 6' do
 		array = [1,2,3]
 		expect(array.my_inject(0){|start,number|start+number}).to eq (6)
+		expect(array.inject(0){|start,number|start+number}).to eq (6)
 	end
 
 	it 'should be able to sum an array of 2,3,4 with the answer 9' do
 		array = [2,3,4]
 		expect(array.my_inject(0){|start,number|start+number}).to eq (9)
+		expect(array.inject(0){|start,number|start+number}).to eq (9)
 	end
 
 	it 'should be able to sum the squares of an array 1,2,3 with the answer 14' do
 		array = [1,2,3]
 		expect(array.my_inject(0){|start,number|start+number**2}).to eq (14)
+		expect(array.inject(0){|start,number|start+number**2}).to eq (14)
 	end
 
-	it "should return nil if an empty array is passed" do
+	it "should return 0 if an empty array is passed" do
 		array = []
-		expect(array.my_inject(0){|start,number|start+number}).to eq nil
+		expect(array.my_inject(0){|start,number|start+number}).to eq 0
+		expect(array.inject(0){|start,number|start+number}).to eq 0
 	end
+
+	it 'should accept no argument before the block' do
+		array = [1,2,3]
+		expect(array.my_inject{|start,number|start+number}).to eq (6)
+		expect(array.inject{|start,number|start+number}).to eq (6)
+	end
+
+	
 
 end
