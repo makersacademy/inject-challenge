643e3b18b28bb20200dcced72aca44cd62d935be
meads58 <meads109@gmail.com>
2015-02-14 13:31:33 +0000

    finished using the each method and stuck on + test for recursive way

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,44 +1,37 @@
 class Array
 
 	def another_inject(*args,&block)
-		copy_of_self = self.dup
-		p copy_of_self
+		copy_of_self = self.clone
 
-			#when !(args[0].is_a Symbol)
-		
-		if args[0].is_a Symbol
-				block = Proc.new { |memo,x| memo.send args[0] , x} 
-				copy_of_self.shift
+		if args[0].is_a? Symbol
+			block = Proc.new { |memo,x| memo.send args[0] , x}
 		elsif args.count == 2
-				block = Proc.new { |memo,x| memo.send args[1] , x} 
-				copy_of_self.delete_at(1)
-		else
+			block = Proc.new { |memo,x| memo.send args[1] , x}
+			copy_of_self.unshift(args[0])
+		elsif args != []
 			copy_of_self.unshift(args[0])
 		end
 
-
-
-		# if args[0].is_a Symbol || args[0].count == 2 
-		# 	block = Proc.new { |memo,x| memo.send :+ , x} 
-		# 	copy_of_self.shift
-		# end
-		
-		# copy_of_self.unshift(args[0]) if args != [] &
 		total = copy_of_self[0]
-		p copy_of_self
 		copy_of_self.shift
 
 		copy_of_self.each do |element|
-		 total = block.call(total, element)
-		end 
+			total = block.call(total, element)
+		end
+
 		total
+
 	end
 
 	def inject_recursion(&block)
-
-
-
+		total, *arr = self
+		if arr != []
+			total = block.call(total, arr[0]) + arr.inject_recursion(&block)
+		end
+		total #+ block.call(total, arr[0])
 	end
+
+
 
 
 end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -1,14 +1,14 @@
 require 'inject'
 
-describe Array do 
+describe Array do
 
 	arr = [1,2,3,4]
 	arr_neg = [-2,-1,1,2]
 
-	describe 'Inject with each method' do
+	describe 'Inject using the each method approach' do
 
 		context 'with only a block' do
-		
+
 			it 'the re_inject method for positive multiplication' do
 				expect(arr.inject {|x,y| x * y}).to eq 24
 				expect(arr.another_inject {|x, y| x * y}).to eq 24
@@ -38,20 +38,19 @@ describe Array do
 			end
 		end
 
-		#QUESTION - symbol doesn't work with & in front is this a ruby 
 		context 'with a symbol' do
 
 			it 'the re_inject method with a symbol block and no arguments' do
-				expect(arr.inject(&:+)).to eq 10
-				expect(arr.another_inject(3,:+)).to eq 10
+				expect(arr.inject(:+)).to eq 10
+				expect(arr.another_inject(:+)).to eq 10
 			end
 		end
 
 		context 'with a symbol and argument' do
-			
+
 			it 'the re_inject method with a symbol block and 3 as an argument' do
-				#expect(arr.inject(3,&:+)).to eq 13
-				expect(arr.another_inject(3,&:+)).to eq 13
+				expect(arr.inject(3,:+)).to eq 13
+				expect(arr.another_inject(3,:+)).to eq 13
 			end
 		end
 	end
@@ -59,10 +58,15 @@ describe Array do
 	describe 'Using recusrion' do
 
 		context 'with only a block' do
-		
+
 			it 'the re_inject method for positive multiplication' do
 				expect(arr.inject {|x,y| x * y}).to eq 24
 				expect(arr.inject_recursion {|x, y| x * y}).to eq 24
+			end
+			#QUESTION - Stuck here, keep on getting 19, need to go over logic agin.
+				it 'the re_inject method for positive multiplication' do
+				expect(arr.inject {|x,y| x + y}).to eq 10
+				expect(arr.inject_recursion {|x, y| x + y}).to eq 10
 			end
 		end
 	end
