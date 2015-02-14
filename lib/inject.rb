94d0ea09a1136031d72fddec459a4f52a956c55a
meads58 <meads109@gmail.com>
2015-02-14 11:40:50 +0000

    commit to push and work at home

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,14 +1,45 @@
 class Array
 
-	def reinject(*args,&block)
-		self.unshift(args[0]) if args != []
-		total = self[0]
-		self.shift
-		self.each do |element|
-		 total = block.call(total, element)
+	def another_inject(*args,&block)
+		copy_of_self = self.dup
+		p copy_of_self
+
+			#when !(args[0].is_a Symbol)
+		
+		if args[0].is_a Symbol
+				block = Proc.new { |memo,x| memo.send args[0] , x} 
+				copy_of_self.shift
+		elsif args.count == 2
+				block = Proc.new { |memo,x| memo.send args[1] , x} 
+				copy_of_self.delete_at(1)
+		else
+			copy_of_self.unshift(args[0])
 		end
+
+
+
+		# if args[0].is_a Symbol || args[0].count == 2 
+		# 	block = Proc.new { |memo,x| memo.send :+ , x} 
+		# 	copy_of_self.shift
+		# end
+		
+		# copy_of_self.unshift(args[0]) if args != [] &
+		total = copy_of_self[0]
+		p copy_of_self
+		copy_of_self.shift
+
+		copy_of_self.each do |element|
+		 total = block.call(total, element)
+		end 
 		total
 	end
+
+	def inject_recursion(&block)
+
+
+
+	end
+
 
 end
 
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -3,21 +3,68 @@ require 'inject'
 describe Array do 
 
 	arr = [1,2,3,4]
+	arr_neg = [-2,-1,1,2]
 
-	it 'base case with real inject method' do
-		expect(arr.inject(3) {|x,y| x * y}).to eq 72
+	describe 'Inject with each method' do
+
+		context 'with only a block' do
+		
+			it 'the re_inject method for positive multiplication' do
+				expect(arr.inject {|x,y| x * y}).to eq 24
+				expect(arr.another_inject {|x, y| x * y}).to eq 24
+			end
+		end
+
+		context 'with a block and argument' do
+
+			it 'the re_inject method with subtraction' do
+				expect(arr.inject(1) {|x, y| x - y}).to eq -9
+				expect(arr.another_inject(1) {|x, y| x - y}).to eq -9
+			end
+
+			it 'the re_inject method with the spaceship' do
+				expect(arr.inject(1) {|x,y| x <=> y}).to eq -1
+				expect(arr.another_inject(1) {|x,y| x <=> y}).to eq -1
+			end
+
+			it 'the re_inject method for negative multiplication' do
+				expect(arr_neg.inject(3) {|x, y| x * y}).to eq 12
+				expect(arr_neg.another_inject(3) {|x, y| x * y}).to eq 12
+			end
+
+			it 'the re_inject method with subtraction' do
+				expect(arr_neg.inject(1) {|x, y| x - y}).to eq 1
+				expect(arr_neg.another_inject(1) {|x, y| x - y}).to eq 1
+			end
+		end
+
+		#QUESTION - symbol doesn't work with & in front is this a ruby 
+		context 'with a symbol' do
+
+			it 'the re_inject method with a symbol block and no arguments' do
+				expect(arr.inject(&:+)).to eq 10
+				expect(arr.another_inject(3,:+)).to eq 10
+			end
+		end
+
+		context 'with a symbol and argument' do
+			
+			it 'the re_inject method with a symbol block and 3 as an argument' do
+				#expect(arr.inject(3,&:+)).to eq 13
+				expect(arr.another_inject(3,&:+)).to eq 13
+			end
+		end
 	end
 
-	it 'the re_inject method' do
-			expect(arr.reinject(3) {|x, y| x * y}).to eq 72
-	end
+	describe 'Using recusrion' do
 
-	it 'the re_inject method with subtraction' do
-			expect(arr.reinject(1) {|x, y| x - y}).to eq -9
-	end
-
-	it 'the re_inject method with the spaceship' do
-		expect(arr.inject(1) {|x,y| x <=> y}).to eq -1
+		context 'with only a block' do
+		
+			it 'the re_inject method for positive multiplication' do
+				expect(arr.inject {|x,y| x * y}).to eq 24
+				expect(arr.inject_recursion {|x, y| x * y}).to eq 24
+			end
+		end
 	end
 
 end
