b1e216c708b0291b87e3dadfa0c99716730311e5
gabe <GabeMaker@users.noreply.github.com>
2015-02-14 15:17:34 +0000

    test pass for string concatenation

--- spec/diyinject_spec.rb
+++ spec/diyinject_spec.rb
@@ -4,14 +4,23 @@ describe Array do
 
   describe "Array#diy_inject" do
 
-    it "can sum some numbers" do
-      expect([1,2,3,4].diy_inject { |sum, n| sum + n }).to eq 10
-    end
+    context "addition" do
 
-    it "can sum other numbers" do
-      expect([5,6,7,8].diy_inject { |sum, n| sum + n }).to eq 26
-    end
+      it "can sum some numbers" do
+        expect([1,2,3,4].diy_inject { |sum, n| sum + n }).to eq 10
+      end
 
+      it "can sum other numbers" do
+        expect([5,6,7,8].diy_inject { |sum, n| sum + n }).to eq 26
+      end
+
+      it "can concatenate strings" do
+        str_array = %w(hello my name is gabe)
+        expect(str_array.diy_inject {|sum, n| sum + n }).to eq "hellomynameisgabe"
+      end
+
+    end
+  
   end
 
 end
