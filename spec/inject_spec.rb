92bf6c25081b77d104414e3d67057de91033f012
Diego <diegoromero.audio@gmail.com>
2015-02-15 13:34:10 +0000

    first commit

---
+++ spec/inject_spec.rb
@@ -0,0 +1,14 @@
+require './lib/Array.rb'
+
+describe Array do
+
+	let(:array){Array.new}
+
+	it 'should be able to pass an array'
+	# expect(array)
+
+end
+
+# arr = []
+# arr << [2, 3, 4].inject(0 + 1) { |result, element| result + element }
+# puts arr 
