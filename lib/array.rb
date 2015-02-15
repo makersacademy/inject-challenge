c9b0a805108755ac79f76b39a0ad327737138647
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-15 00:02:59 +0000

    Finally understood how I get a block as proc into my method and how to call it. Sum-up does already work

--- lib/array.rb
+++ lib/array.rb
@@ -10,5 +10,27 @@ class Array
     return @memo.last  
   end
 
+  def my_sum_up
+    memo = 0
+    index = 0
+    until index == self.size
+      memo = self[index] + memo
+      index += 1
+    end
+    memo
+  end
+
+  def get_proc_inside(&proc)
+    proc.call(2, 2)
+  end
+
+  def my_inject(&proc)
+    array = self
+    memo ||=array.shift
+    array.size.times {memo = proc.call(memo,array.shift)}
+    memo
+  end
+  
+
 end
 
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,6 +3,7 @@ require 'array'
 describe Array do
 
     array = [1,2,3,4]
+    let(:memo){0}
 
   it "should transform each element of a given array" do
     expect(array.add_each_three).to eq([4,5,6,7])
@@ -14,7 +15,21 @@ describe Array do
   
   end
 
+  it "can sum up all values without map" do
+    array = [2,2,3,4]
+    expect(array.my_sum_up).to eq(11)
+    
+  end
+
+  it "can use the code-block I pass in" do
+    expect(array.get_proc_inside{|memo, item| memo + item}).to eq(4)
+  end
   
+
+  it "can add each of the array to memo" do
+    expect([1, 2, 3, 4].my_inject{|memo, item| memo + item}).to eq 10
+  end
+
 end
 
 
