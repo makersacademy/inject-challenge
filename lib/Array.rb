9281d2a979677f6087e09e0f2d24202d46cb44a4
Diego <diegoromero.audio@gmail.com>
2015-02-15 17:11:37 +0000

    got to make it work with add, deduct, and multiply, missing divide

--- lib/Array.rb
+++ lib/Array.rb
@@ -1,3 +1,11 @@
-class Array 
+class Array
+
+	def update_with(array)
+		each do |element| 
+			array = yield array,element
+		end
+		p array
+	end
+
 
 end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -1,14 +1,20 @@
-require './lib/Array.rb'
+require 'array'
 
 describe Array do
+	it 'can update each of its elements'do
+	expect([1,2,3,4].update_with(0) {|array, element| array * element}).to eq (24)
+	end
 
-	let(:array){Array.new}
+	it 'can sum each of its elements' do
+		expect([1,2,3,4].update_with(0){|array, element| array+element}).to eq 10
+	end
 
-	it 'should be able to pass an array'
-	# expect(array)
+	it 'can deduct each of its own elements' do
+		expect([1,2,3,4].update_with(0){|array, element| array-element}).to eq -10
+	end
+
+	# it 'can divide each of its own elements' do
+	# 	expect([60,30].update_with(0){|array, element| array/element}).to eq 2
+	# end
 
 end
-
-# arr = []
-# arr << [2, 3, 4].inject(0 + 1) { |result, element| result + element }
-# puts arr 
