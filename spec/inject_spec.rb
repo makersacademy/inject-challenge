b276df1a8db0ad5fdc4ff3aba9930904ce3b998d
SebastienPires <pires.sebastien@gmail.com>
2015-02-15 12:02:41 +0000

    First commit and method working

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -3,7 +3,15 @@ require 'inject'
 describe Array do 
 
 		
-		it 'sum numbers' do
-			expect([1,2,3,4,5].sebastien_injection(&:*)).to eq(120)
+		it 'Sum some numbers' do
+			expect([1,2,3,4,5].sebastien_injection(&:+)).to eq(15)
 		end
+
+		it 'Can find the shortest word' do
+      shortest_word = ["lion", "dog", "computer" "banana"].sebastien_injection do |a, w|
+        a.length < w.length ? a : w
+      end
+      expect(shortest_word).to eq("dog")
+    end
+
 end
