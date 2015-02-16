976b0b9f0a5a48afc8fae45e3c267b3b53f631d2
Sebastien Pires <pires.sebastien@gmail.com>
2015-02-16 00:04:48 +0000

    Update inject.rb

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,7 +2,7 @@ class Array
 
 	def sebastien_injection(a= nil, b=nil)
 		injectme = a ? b : self.shift
-					self.map {|x| injectme = yield(injectme , x)}
+		self.map {|x| injectme = yield(injectme , x)}
 		return injectme	
 	end
 end
