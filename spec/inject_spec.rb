6397a977e57af512dddfffb88393df18410f7ebb
Sebastien Pires <pires.sebastien@gmail.com>
2015-02-16 00:05:45 +0000

    Update inject_spec.rb

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -1,12 +1,10 @@
 require 'inject'
 
 describe Array do 
-
-		
 	it 'Sum some numbers' do
 		expect([1,2,3,4,5].sebastien_injection(&:+)).to eq(15)
 	end
-
+	
 	it 'Can find the shortest word' do
       		shortest_word = ["lion", "dog", "computer" "banana"].sebastien_injection do |a, w|
         	a.length < w.length ? a : w
