40c23f62df648d978a99eda2afe96d937b736d8b
Vanessa <virgitti@gmail.com>
2015-02-16 08:42:23 +0000

    array class

--- lib/array.rb
+++ lib/array.rb
@@ -2,7 +2,7 @@ class Array
 
 
 	def injection(number,operation)
-		number.reduce(:+) if  "sum"
+		number.reduce(:+) if "sum"
 		number.reduce(1,:*) if "product"
 	end
 
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -6,7 +6,9 @@ describe Array do
 	let(:array){Array.new}
 
 	it 'can sum 2 number up' do
+		# allow(array).to receive(:injection){"sum"}
 		number1 = [1,2]
+		operation = "sum"
 		expect(array.injection(number1,"sum")).to eq (3)
 	end
 
