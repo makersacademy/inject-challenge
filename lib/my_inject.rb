febe63364f6e57652fa88e9cf3dc8aa3aee85526
sanjsanj <sanjsanj@hotmail.com>
2015-03-27 17:31:04 +0000

    Sort of getting somewhere

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,36 +1,20 @@
 class Array
-  def my_inject(initial=undefined, sym=undefined)
-    if !block_given? or !undefined.equal?(sym)
-      if undefined.equal?(sym)
-        sym = initial
-        initial = undefined
-      end
-
-      # Do the sym version
-
-      sym = sym.to_sym
-
-      each do
-        o = Rubinius.single_block_arg
-        if undefined.equal? initial
-          initial = o
-        else
-          initial = initial.__send__(sym, o)
-        end
-      end
-
-      # Block version
-    else
-      each do
-        o = Rubinius.single_block_arg
-        if undefined.equal? initial
-          initial = o
-        else
-          initial = yield(initial, o)
-        end
-      end
+  def my_inject starting = 0
+    @result ||= starting + self.first
+    self[1..-1].each do |value|
+      @result = yield(@result, value)
     end
-
-    undefined.equal?(initial) ? nil : initial
+    @result
   end
 end
+
+# class Array
+#   def my_inject starting = self.first
+#     starting ? thing = self : thing = self[1..-1]
+#     @result = starting
+#     thing.each do |value|
+#       @result = yield(@result, value)
+#     end
+#     @result
+#   end
+# end
