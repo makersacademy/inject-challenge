422eeed8a9080f94b9fe1e416d95b8072ffbca29
Richard Lynch <rjlynch86@googlemail.com>
2015-03-27 16:53:33 +0000

    makes my_inject robust

--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -97,20 +97,29 @@ describe Array do
     end
 
     it 'can subtract' do
-      expect([1, 2, 3, 4].inject(10, :-)).to eq 0
+      expect([1, 2, 3, 4].my_inject(10, :-)).to eq 0
     end
 
     it 'can multipy' do
-      expect([1, 2, 3, 4].inject(10, :*)).to eq 240
+      expect([1, 2, 3, 4].my_inject(10, :*)).to eq 240
     end
 
     it 'can divide' do
-      expect([1.0, 2.0, 3.0, 4.0].inject(10, :/)).to eq 0.4166666666666667
+      expect([1.0, 2.0, 3.0, 4.0].my_inject(10, :/)).to eq 0.4166666666666667
     end
 
     it 'can add with a string' do
-      expect(['h', 'e', 'l', 'l', 'o'].inject { |mem, el| mem + el }).to eq 'hello'
+      expect(['h', 'e', 'l', 'l', 'o'].my_inject { |mem, el| mem + el }).to eq 'hello'
     end
+
+  end
+
+  context 'extra test' do
+
+      it 'passes the Mihai test' do
+        data = [{a: 1}, {b: 2}, {c: 3}]
+        expect(data.inject(:merge)).to eq(data.my_inject(:merge))
+      end
 
   end
 
