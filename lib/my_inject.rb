974f13b770208658a993b76d9f92a4d8b1bb10ae
sanjsanj <sanjsanj@hotmail.com>
2015-03-27 20:58:53 +0000

    Write complete suite of tests

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,12 +1,12 @@
-class Array
-  def my_inject starting = 0
-    @result ||= starting + self.first
-    self[1..-1].each do |value|
-      @result = yield(@result, value)
-    end
-    @result
-  end
-end
+# class Array
+#   def my_inject starting = 0
+#     @result ||= starting + self.first
+#     self[1..-1].each do |value|
+#       @result = yield(@result, value)
+#     end
+#     @result
+#   end
+# end
 
 # class Array
 #   def my_inject starting = self.first
@@ -16,5 +16,51 @@ end
 #       @result = yield(@result, value)
 #     end
 #     @result
+#   end
+# end
+
+class Array
+  def my_inject starting = nil
+    if starting != nil
+      @result = starting
+      each do |value|
+        @result = yield(@result, value)
+      end
+      @result
+    else
+      @result = self.first
+      self[1..-1].each do |value|
+        @result = yield(@result, value)
+      end
+      @result
+    end
+  end
+end
+
+# class Array
+#   def my_inject args = 0
+#     # p 'start'
+#     if args
+#       # p 'does have args'
+#       if args == args.to_sym
+#         # p 'args are sym'
+#       else
+#         # p 'args are not sym'
+#         @result = args
+#         each do |value|
+#           @result = yield(@result, value)
+#         end
+#         @result
+#       end
+#       # p args
+#     else
+#       # p 'does not have args'
+#       args = self.first
+#       @result = args
+#       self[1..-1].each do |value|
+#         @result = yield(@result, value)
+#       end
+#       @result
+#     end
 #   end
 # end
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -1,6 +1,6 @@
 require 'my_inject'
 
-describe 'Array' do
+describe 'my_inject' do
   it 'can add' do
     expect([1, 2, 3].inject { |sum, n| sum + n }).to eq 6
     expect([1, 2, 3].my_inject { |sum, n| sum + n }).to eq 6
@@ -9,6 +9,16 @@ describe 'Array' do
   it 'can multiply' do
     expect([1, 2, 3].inject { |sum, n| sum * n }).to eq 6
     expect([1, 2, 3].my_inject { |sum, n| sum * n }).to eq 6
+  end
+
+  it 'can subtract' do
+    expect([10, 5, 2].inject { |sum, n| sum - n }).to eq 3
+    expect([10, 5, 2].my_inject { |sum, n| sum - n }).to eq 3
+  end
+
+  it 'can divide' do
+    expect([1, 2, 3].inject { |sum, n| sum / n }).to eq 0
+    expect([1, 2, 3].my_inject { |sum, n| sum / n }).to eq 0
   end
 
   it 'can add with starting point' do
@@ -21,13 +31,33 @@ describe 'Array' do
     expect([1, 2, 3].my_inject(10) { |sum, n| sum * n }).to eq 60
   end
 
-  it 'can add with a symbol' do
-    expect([1, 2, 3].inject(:+)).to eq 6
-    # expect([1, 2, 3].my_inject(:+)).to eq 6
+  it 'can subtract with a starting point' do
+    expect([1, 2, 3].inject(10) { |sum, n| sum - n }).to eq 4
+    expect([1, 2, 3].my_inject(10) { |sum, n| sum - n }).to eq 4
   end
 
-  it 'can multiply with a starting point' do
-    expect([1, 2, 3].inject(10) { |sum, n| sum * n }).to eq 60
-    # expect([1, 2, 3].my_inject(10) { |sum, n| sum * n }).to eq 60
+  it 'can divide with a starting point' do
+    expect([2, 2, 2].inject(80) { |sum, n| sum / n }).to eq 10
+    expect([2, 2, 2].my_inject(80) { |sum, n| sum / n }).to eq 10
+  end
+
+  it 'can add with a symbol' do
+    expect([1, 2, 3].inject(:+)).to eq 6
+    expect([1, 2, 3].my_inject(:+)).to eq 6
+  end
+
+  it 'can multiply with a symbol' do
+    expect([1, 2, 3].inject(:*)).to eq 6
+    expect([1, 2, 3].my_inject(:*)).to eq 6
+  end
+
+  it 'can subtract with a symbol' do
+    expect([50, 20, 3].inject(:-)).to eq 27
+    expect([50, 20, 3].my_inject(:-)).to eq 27
+  end
+
+  it 'can divide with a symbol' do
+    expect([80, 2, 2].inject(:/)).to eq 20
+    expect([80, 2, 2].my_inject(:/)).to eq 20
   end
 end
