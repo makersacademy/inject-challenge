13bd202244341f8ae6abfac0ece03495304245f2
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-27 18:48:54 +0000

    refactored test suite

--- spec/array_deflate_spec.rb
+++ spec/array_deflate_spec.rb
@@ -3,53 +3,63 @@ require 'deflate'
 
 describe Array do
 
-  it 'can return a sum of its contents' do
-    expect([1, 2, 3].deflate { |sum, num| sum += num }).to eq 6
-  end
+  describe '#deflate' do
 
-  it 'can return the result of reducing its contents' do
-    expect([1, 2, 3].deflate { |sum, num| sum -= num }).to eq(-4)
-  end
+    let(:array) { [1, 2, 3] }
 
-  it 'can return the result of multiplying its contents' do
-    expect([1, 2, 3].deflate { |sum, num| sum *= num }).to eq(6)
-  end
-
-  it 'can add its contents to a starting number' do
-    expect([1, 2, 3].deflate(10) { |sum, num| sum += num }).to eq 16
-  end
-
-  it 'can return the result of reducing its contents from a starting number' do
-    expect([1, 2, 3].deflate(10) { |sum, num| sum -= num }).to eq 4
-  end
-
-  it 'returns the result of multiplying its contents from a starting number' do
-    expect([1, 2, 3].deflate(10) { |sum, num| sum *= num }).to eq(60)
-  end
-
-  it 'can take a symbol and apply that to its contents' do
-    expect([1, 2, 3].deflate :+).to eq(6)
-
-    expect([1, 2, 3].deflate :*).to eq(6)
-
-    expect([1, 2, 3].deflate :-).to eq(-4)
-  end
-
-  it 'can take a symbol and starting number' do
-    expect([1, 2, 3].deflate(10, :+)).to eq(16)
-  end
-
-  it 'can find the longest word' do
-    longest = %w( cat sheep bear ).deflate do |memo, word|
-      memo.length > word.length ? memo : word
+    context 'when passed a block' do
+      it 'sums contents' do
+        expect(array.deflate { |sum, num| sum += num }).to eq 6
+      end
+      it 'reduces contents' do
+        expect(array.deflate { |sum, num| sum -= num }).to eq(-4)
+      end
+      it 'multiplies contents' do
+        expect(array.deflate { |sum, num| sum *= num }).to eq(6)
+      end
     end
-    expect(longest).to eq 'sheep'
-  end
 
-  it 'can be used for conctatonation' do
-    concatonated = %w( words to be concatonated).deflate do |final, word|
-      final += word
+    context 'when passed a block and a initial value' do
+
+      it 'adds its contents to initial value' do
+        expect(array.deflate(10) { |sum, num| sum += num }).to eq 16
+      end
+      it 'subtracts its contents from initial value' do
+        expect(array.deflate(10) { |sum, num| sum -= num }).to eq 4
+      end
+      it 'multiplies its contents from initial value' do
+        expect(array.deflate(10) { |sum, num| sum *= num }).to eq(60)
+      end
     end
-    expect(concatonated).to eq 'wordstobeconcatonated'
+
+    context 'when passed a method' do
+
+      it 'uses it to reduce its contents' do
+        expect(array.deflate :+).to eq(6)
+
+        expect([1, 2, 3].deflate :*).to eq(6)
+
+        expect([1, 2, 3].deflate :-).to eq(-4)
+      end
+      it 'uses it to reduce its contents from an initial value' do
+        expect(array.deflate(10, :+)).to eq(16)
+      end
+    end
+
+    context 'when given a string' do
+
+      it 'can be used find the longest word' do
+        longest = %w( cat sheep bear ).deflate do |memo, word|
+          memo.length > word.length ? memo : word
+        end
+        expect(longest).to eq 'sheep'
+      end
+      it 'can be used to concatenate' do
+        concatenated = %w( words to be concatenated).deflate do |final, word|
+          final += word
+        end
+        expect(concatenated).to eq 'wordstobeconcatenated'
+      end
+    end
   end
 end
