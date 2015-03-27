f9e57ade7773552286619190308f362805b71d2f
Richard Lynch <rjlynch86@googlemail.com>
2015-03-27 17:18:59 +0000

    one offense left

--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -10,8 +10,9 @@ describe Array do
     end
 
     it 'can subtract' do
-      expect([1, 2, 3, 4].inject { |mem, el| mem - el }).to eq -8
-      expect([1, 2, 3, 4].my_inject { |mem, el| mem - el }).to eq -8
+      neg_8 = (-8)
+      expect([1, 2, 3, 4].inject { |mem, el| mem - el }).to eq neg_8
+      expect([1, 2, 3, 4].my_inject { |mem, el| mem - el }).to eq neg_8
     end
 
     it 'can multipy' do
@@ -20,19 +21,21 @@ describe Array do
     end
 
     it 'can divide' do
-      expect([1.0, 2.0, 3.0, 4.0].inject { |mem, el| mem / el }).to eq 0.041666666666666664
-      expect([1.0, 2.0, 3.0, 4.0].my_inject { |mem, el| mem / el }).to eq 0.041666666666666664
+      expect([1.0, 2.0, 3.0, 4.0].inject { |mem, el| mem / el })
+        .to eq 0.041666666666666664
+      expect([1.0, 2.0, 3.0, 4.0].my_inject { |mem, el| mem / el })
+        .to eq 0.041666666666666664
     end
 
     it 'can add with a string' do
-      expect(['h', 'e', 'l', 'l', 'o'].inject { |mem, el| mem + el }).to eq 'hello'
-      expect(['h', 'e', 'l', 'l', 'o'].my_inject { |mem, el| mem + el }).to eq 'hello'
+      expect(%w(h e l l o).inject { |mem, el| mem + el }).to eq 'hello'
+      expect(%w(h e l l o).my_inject { |mem, el| mem + el }).to eq 'hello'
     end
 
   end
 
   context 'with a block and an argument' do
-    
+
     it 'can add' do
       expect([1, 2, 3, 4].inject(10) { |mem, el| mem + el }).to eq 20
       expect([1, 2, 3, 4].my_inject(10) { |mem, el| mem + el }).to eq 20
@@ -49,13 +52,15 @@ describe Array do
     end
 
     it 'can divide' do
-      expect([1.0, 2.0, 3.0, 4.0].inject(10) { |mem, el| mem / el }).to eq 0.4166666666666667
-      expect([1.0, 2.0, 3.0, 4.0].my_inject(10) { |mem, el| mem / el }).to eq 0.4166666666666667
+      expect([1.0, 2.0, 3.0, 4.0].inject(10) { |mem, el| mem / el })
+        .to eq 0.4166666666666667
+      expect([1.0, 2.0, 3.0, 4.0].my_inject(10) { |mem, el| mem / el })
+        .to eq 0.4166666666666667
     end
 
     it 'can add with a string' do
-      expect([' ', 'b', 'y', 'e' ].inject('hello') { |mem, el| mem + el }).to eq 'hello bye'
-      expect([' ', 'b', 'y', 'e' ].my_inject('hello') { |mem, el| mem + el }).to eq 'hello bye'
+      expect(%w(b y e).inject('hi') { |mem, el| mem + el }).to eq 'hibye'
+      expect(%w(b y e).my_inject('hi') { |mem, el| mem + el }).to eq 'hibye'
     end
 
   end
@@ -68,8 +73,9 @@ describe Array do
     end
 
     it 'can subtract' do
-      expect([1, 2, 3, 4].inject :-).to eq -8
-      expect([1, 2, 3, 4].my_inject :-).to eq -8
+      neg_8 = (-8)
+      expect([1, 2, 3, 4].inject :-).to eq neg_8
+      expect([1, 2, 3, 4].my_inject :-).to eq neg_8
     end
 
     it 'can multipy' do
@@ -78,13 +84,15 @@ describe Array do
     end
 
     it 'can divide' do
-      expect([1.0, 2.0, 3.0, 4.0].inject :/).to eq 0.041666666666666664
-      expect([1.0, 2.0, 3.0, 4.0].my_inject :/).to eq 0.041666666666666664
+      expect([1.0, 2.0, 3.0, 4.0].inject :/)
+        .to eq 0.041666666666666664
+      expect([1.0, 2.0, 3.0, 4.0].my_inject :/)
+        .to eq 0.041666666666666664
     end
 
     it 'can add with a string' do
-      expect(['h', 'e', 'l', 'l', 'o'].inject :+).to eq 'hello'
-      expect(['h', 'e', 'l', 'l', 'o'].my_inject :+).to eq 'hello'
+      expect(%w(h e l l o).inject :+).to eq 'hello'
+      expect(%w(h e l l o).my_inject :+).to eq 'hello'
     end
 
   end
@@ -109,17 +117,17 @@ describe Array do
     end
 
     it 'can add with a string' do
-      expect(['h', 'e', 'l', 'l', 'o'].my_inject { |mem, el| mem + el }).to eq 'hello'
+      expect(%w(h e l l o).my_inject { |mem, el| mem + el }).to eq 'hello'
     end
 
   end
 
   context 'extra test' do
 
-      it 'passes the Mihai test' do
-        data = [{a: 1}, {b: 2}, {c: 3}]
-        expect(data.inject(:merge)).to eq(data.my_inject(:merge))
-      end
+    it 'passes the Mihai test' do
+      data = [{ a: 1 }, { b: 2 }, { c: 3 }]
+      expect(data.inject(:merge)).to eq(data.my_inject(:merge))
+    end
 
   end
 
