974471e12ec07343c5440c91f4513a37eda77993
Mark Pokorny <iridium193@gmail.com>
2015-03-27 15:33:31 +0000

    Spec file updated to test for multiplication of array elements.

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -4,8 +4,14 @@ describe Array do
   it 'uses inject to add elements together to make a sum' do
     expect([1, 2, 3, 4, 5].inject { |sum, num| sum + num }).to eq 15
   end
-
   it 'uses custom_inject to add elements together to make a sum' do
     expect([1, 2, 3, 4, 5].custom_inject { |sum, num| sum + num }).to eq 15
+  end
+
+  it 'uses inject to multiply elements together to make a product' do
+    expect([1, 2, 3, 4, 5].inject { |sum, num| sum * num }).to eq 120
+  end
+  it 'uses custom_inject to multiply elements together to make a product' do
+    expect([1, 2, 3, 4, 5].custom_inject { |sum, num| sum * num }).to eq 120
   end
 end
