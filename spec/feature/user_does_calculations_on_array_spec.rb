68002bfd4c007118fd90d47bf9f006e2103c80b6
Jade King <dev@jadeking.co.uk>
2015-03-27 19:13:21 +0000

    updated mistake add expect calculate array content test

--- spec/feature/user_does_calculations_on_array_spec.rb
+++ spec/feature/user_does_calculations_on_array_spec.rb
@@ -1,6 +1,7 @@
 require 'capybara/rspec'
 feature 'does calculations on array content' do
   scenario 'calculate sum of array' do
-    [1, 2, 3].my_inject(0) { |num, sum| sum += num }
+    result = [1, 2, 3].my_inject(0) { |num, sum| sum += num }
+    expect(result).to eq 6
   end
 end
