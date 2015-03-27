0291605bfc9ebfb4779fbf6ceb39d86a74cfa811
Joseph Knowles <joejknowles@gmail.com>
2015-03-27 15:18:43 +0000

    created recursive version of inject

--- lib/my_inject_rec.rb
+++ lib/my_inject_rec.rb
@@ -1,21 +1,25 @@
 class Array
-  def my_inject_rec(*args)
+  def my_inject_rec(*args, &block)
     start, accumulator = start_and_accumulator_rec(args)
-    if block_given?
-      (start...length).each do |num|
-        accumulator = yield(accumulator, self[num])
-      end
+    if block
+      accumulator = run_block_rec(start, accumulator, block)
     else
       accumulator = run_symbol_rec(start, accumulator, args[-1])
     end
     accumulator
   end
 
+  def run_block_rec(start, accumulator, block)
+    accumulator = block.call(accumulator, self[start])
+    start += 1
+    accumulator = run_block_rec(start, accumulator, block) if start < length
+    accumulator
+  end
+
   def start_and_accumulator_rec(args)
     start = 1
-    if args[0].nil? || args[0].is_a?(Symbol)
-      accumulator = self[0]
-    else
+    accumulator = self[0]
+    unless args[0].nil? || args[0].is_a?(Symbol)
       start = 0
       accumulator = args[0]
     end
