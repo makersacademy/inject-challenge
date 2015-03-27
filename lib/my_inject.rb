fb3884b6359328fd863b92631b38b54271be7e39
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-27 20:14:21 +0000

    it can add'

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,5 +1,6 @@
 class Array
-  def my_inject starting, symbol, &block
-    
+  def my_inject starting=0, *symbol, &block
+    @starting=starting
+    self.map.each { |num| @starting+= num }.last
   end
 end
