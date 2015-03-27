0f126b4203dc8f50fd67db7506a3d67b5b012f5e
armi1189 <armi1189@gmail.com>
2015-03-27 21:32:24 +0000

    New tests and refactor

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,31 +1,23 @@
 class Array
+
   def my_inject sum = nil, symbol = nil, &block
     if sum.is_a? Symbol
-      total = self.first
-      self.shift
-      if sum == :+
-        self.each { |x| total = total + x }
-      elsif sum == :*
-        self.each { |x| total = total * x }
-      end
+      total = shift
+      each { |x| total = total.send(sum, x) }
       total
     else
       if sum.nil? && symbol.nil?
-        sum = self.first
-        self.shift
-        self.each { |number| sum = block.call(sum, number) }
+        sum = shift
+        each { |number| sum = block.call(sum, number) }
         sum
       elsif symbol.nil?
-        self.each { |number| sum = block.call(sum, number) }
+        each { |number| sum = block.call(sum, number) }
         sum
       else
-        if symbol == :+
-          self.each { |x| sum = sum + x }
-        elsif symbol == :*
-          self.each { |x| sum = sum * x }
-        end
+        each { |x| sum = sum.send(symbol, x) }
         sum
       end
     end
   end
+
 end
