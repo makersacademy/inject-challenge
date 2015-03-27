37666dcb4e1261a9e87eaacfe20d5cb22ba8a174
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-27 19:05:25 +0000

    Inject method implemented and tests passing

--- lib/my_array.rb
+++ lib/my_array.rb
@@ -1,3 +1,18 @@
 class MyArray < Array
+  def initialize(array)
+    @array = array
+  end
 
+  def inject(arg1 = 0, arg2 = nil)
+    memo = arg1
+    if arg1.is_a? Symbol
+      memo = 0
+      @array.each { |value| memo = memo.send(arg1, value) }
+    elsif arg2.is_a? Symbol
+      @array.each { |value| memo = memo.send(arg2, value) }
+    else
+      @array.each { |value| memo = yield(memo, value) }
+    end
+    memo
+  end
 end
--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -1,24 +1,26 @@
 require 'my_array'
 
 describe MyArray do
+  let(:array) { (1..5) }
+  let(:subject) { described_class.new(array) }
   it 'can sum elements in an array together using a block' do
-    expect([1, 2, 3, 4, 5].inject { |sum, item| sum += item }).to eq 15
+    expect(array.inject { |sum, item| sum += item }).to eq 15
+    expect(subject.inject { |sum, item| sum += item }).to eq 15
   end
 
   it 'can sum elements in an array using a block, with a start point' do
-    expect([1, 2, 3, 4, 5].inject(5) { |sum, item| sum += item }).to eq 20
+    expect(array.inject(5) { |sum, item| sum += item }).to eq 20
+    expect(subject.inject(5) { |sum, item| sum += item }).to eq 20
   end
 
   it 'can sum elements in an array using a symbol' do
-    expect([1, 2, 3, 4, 5].inject(:+)).to eq 15
+    expect(array.inject(:+)).to eq 15
+    expect(subject.inject(:+)).to eq 15
   end
 
-  it 'can multiply elements in an array using a symbol' do
-    expect([1, 2, 3, 4, 5].inject(5, :*)).to eq 600
-  end
-
-  it 'can concatenate an array to a string' do
-    expect(("A".."E").inject(:+)).to eq "ABCDE"
+  it 'can multiply elements in an array using a symbol and a start point' do
+    expect(array.inject(5, :*)).to eq 600
+    expect(subject.inject(5, :*)).to eq 600
   end
 
 end
