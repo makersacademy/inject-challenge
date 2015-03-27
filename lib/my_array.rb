7dd9ef7ee2a0b045b7bf9f53cb1a7ec466aa0e8b
Richard Lynch <rjlynch86@googlemail.com>
2015-03-27 16:23:19 +0000

    removed puts

--- lib/my_array.rb
+++ lib/my_array.rb
@@ -1,38 +1,3 @@
-# class Array
-#   def my_inject arg = false, arg_sym =false, &block
-    
-#     add = Proc.new { |mem, el| mem + el }
-#     subtract = Proc.new { |mem, el| mem - el }
-#     multipy = Proc.new { |mem, el| mem * el }
-#     divide = Proc.new { |mem, el| mem / el }
-
-#     case arg
-#     when Symbol
-#       blocks_hash = {
-#         :+ => add,
-#         :- => subtract,
-#         :* => multipy,
-#         :/ => divide
-#       }
-#       total = self.shift
-#       self.each do |el|
-#           total = blocks_hash[arg].call(total, el)
-#       end
-#     else
-#       # when passed a block or normal arg
-#       arg ? total = arg : total = self.shift
-#       self.each do |el|
-#           total = block.call(total, el)
-#       end
-#     end
-#     total
-#   end
-# end
-
-
-# when passed a block or normal arg
-# when passed a Symbol
-# when passed an arg and a symbol
 
 class Array
   def my_inject arg = false, arg_sym = false, &block
@@ -46,11 +11,7 @@ class Array
       :* => multipy,
       :/ => divide
     }
-    puts "arg: #{arg}, arg_sym: #{arg_sym}"
-    puts "block = "
-    p block
     if arg_sym
-      puts 'here'
       total = arg
       self.each do |el|
             total = blocks_hash[arg_sym].call(total, el)
