433d8208bf135b719ab0bb4be06754d9096693c4
Mark Pokorny <iridium193@gmail.com>
2015-03-27 16:39:48 +0000

    Added tests to check for division (current code already implements this).

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -21,4 +21,11 @@ describe Array do
   it 'uses custom_inject to subtract elements to make a difference' do
     expect([1, 2, 3, 4, 5].custom_inject { |sum, num| sum - num }).to eq(-13)
   end
+
+  it 'uses inject to subtract elements to make a difference' do
+    expect([30, 5, 2].inject { |sum, num| sum / num }).to eq 3
+  end
+  it 'uses custom_inject to subtract elements to make a difference' do
+    expect([30, 5, 2].custom_inject { |sum, num| sum / num }).to eq 3
+  end
 end
