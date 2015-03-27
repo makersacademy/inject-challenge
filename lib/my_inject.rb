73494ff3c2bb819ccf78f8e56287c251c4946a68
Joseph Knowles <joejknowles@gmail.com>
2015-03-27 12:16:19 +0000

    Added test for taking in a starting value

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,7 +1,6 @@
 class Array
-  def my_inject
-    sum = self[0]
-    (1...length).each { |num| sum = yield(sum, self[num]) }
-    sum
+  def my_inject(accumulator = self[0])
+    (1...length).each { |num| accumulator = yield(accumulator, self[num]) }
+    accumulator
   end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,12 +1,16 @@
 require 'my_inject'
 
 describe Array do
-  it 'adds numbers in an array' do
+  it 'takes block to add numbers in an array' do
     expect([1, 4, 5].inject { |sum, num| sum + num }).to eq 10
     expect([1, 4, 5].my_inject { |sum, num| sum + num }).to eq 10
   end
-  it 'multiplies numbers in an array' do
+  it 'takes block to multiply numbers in an array' do
     expect([1, 4, 5].my_inject { |sum, num| sum * num }).to eq 20
     expect([1, 4, 5].my_inject { |sum, num| sum * num }).to eq 20
+  end
+  it 'takes block to add numbers in an array with a start number' do
+    expect([1, 4, 5].inject(3) { |sum, num| sum + num }).to eq 13
+    expect([1, 4, 5].my_inject(3) { |sum, num| sum + num }).to eq 13
   end
 end
