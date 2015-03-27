727902f4771f9c646ac05fea00b95b8ef364e1f9
Mark Pokorny <iridium193@gmail.com>
2015-03-27 15:55:54 +0000

    ‘custom_inject’ now multiplies too!

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,6 @@
 class Array
   def custom_inject
-    injector = 0
+    injector = shift # i.e: self.shift (RuboCop is not being friendly!)
     each { |num| injector = yield(injector, num) }
     injector
   end
