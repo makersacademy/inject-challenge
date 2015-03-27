44709565591d9b905a42c92cc2cd2fdbaad9a137
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-27 14:41:28 +0000

    deflate method split into two private methods for handling block/not

--- lib/deflate.rb
+++ lib/deflate.rb
@@ -1,9 +1,27 @@
 class Array
-  def deflate(start_with = nil)
-    start_with.nil? ? starting_index = 1 : starting_index = 0
-    total = start_with || first
-    self[starting_index..-1].each do |num|
-      total = yield(total, num)
+  def deflate(arg = nil, &block)
+    if block_given?
+      deflate_with_block(arg, block)
+    else
+      deflate_without_block(arg)
+    end
+  end
+
+  private
+
+  def deflate_with_block(number_to_begin_with, block)
+    number_to_begin_with.nil? ? start_from_index = 1 : start_from_index = 0
+    total = number_to_begin_with || first
+    self[start_from_index..-1].each do |num|
+      total = block.call(total, num)
+    end
+    total
+  end
+
+  def deflate_without_block(_sym)
+    total = first
+    self[1..-1].each do |num|
+      total *= num
     end
     total
   end
--- spec/array_deflate_spec.rb
+++ spec/array_deflate_spec.rb
@@ -19,12 +19,15 @@ describe Array do
     expect([1, 2, 3].deflate(10) { |sum, num| sum += num }).to eq 16
   end
 
-  # it 'can return the result of reducing its contents' do
-  #   expect([1, 2, 3].deflate { |sum, num| sum -= num }).to eq(-4)
-  # end
+  it 'can return the result of reducing its contents from a starting number' do
+    expect([1, 2, 3].deflate(10) { |sum, num| sum -= num }).to eq 4
+  end
 
-  # it 'can return the result of multiplying its contents' do
-  #   expect([1, 2, 3].deflate { |sum, num| sum *= num }).to eq(6)
-  # end
-  # end
+  it 'returns the result of multiplying its contents from a starting number' do
+    expect([1, 2, 3].deflate(10) { |sum, num| sum *= num }).to eq(60)
+  end
+
+  it 'can take a symbol and apply that to its contents' do
+    expect([1, 2, 3].deflate :+).to eq(6)
+  end
 end
