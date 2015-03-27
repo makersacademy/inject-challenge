53ab849d1eaccf90ce47cac038c0804416606410
Joseph Knowles <joejknowles@gmail.com>
2015-03-27 14:15:36 +0000

    added lots of edge case tests

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,6 +5,10 @@ describe Array do
     expect([1, 4, 5].inject { |sum, num| sum + num }).to eq 10
     expect([1, 4, 5].my_inject { |sum, num| sum + num }).to eq 10
   end
+  it 'takes block to subtract numbers in an array' do
+    expect([1, 4, 5].inject { |sum, num| sum - num }).to eq(-8)
+    expect([1, 4, 5].my_inject { |sum, num| sum - num }).to eq(-8)
+  end
   it 'takes block to multiply numbers in an array' do
     expect([1, 4, 5].my_inject { |sum, num| sum * num }).to eq 20
     expect([1, 4, 5].my_inject { |sum, num| sum * num }).to eq 20
@@ -13,6 +17,10 @@ describe Array do
     expect([1, 4, 5].inject(3) { |sum, num| sum + num }).to eq 13
     expect([1, 4, 5].my_inject(3) { |sum, num| sum + num }).to eq 13
   end
+  it 'takes block to subtract numbers in an array with a start number' do
+    expect([1, 4, 5].inject(3) { |sum, num| sum - num }).to eq(-7)
+    expect([1, 4, 5].my_inject(3) { |sum, num| sum - num }).to eq(-7)
+  end
   it 'takes block to multiply numbers in an array with a start number' do
     expect([1, 4, 5].inject(3) { |sum, num| sum * num }).to eq 60
     expect([1, 4, 5].my_inject(3) { |sum, num| sum * num }).to eq 60
@@ -20,6 +28,10 @@ describe Array do
   it 'takes symbol to add numbers in an array' do
     expect([1, 4, 5].inject(:+)).to eq 10
     expect([1, 4, 5].my_inject(:+)).to eq 10
+  end
+  it 'takes symbol to subtract numbers in an array' do
+    expect([1, 4, 5].inject(:-)).to eq(-8)
+    expect([1, 4, 5].my_inject(:-)).to eq(-8)
   end
   it 'takes symbol to multiply numbers in an array' do
     expect([1, 4, 5].inject(:*)).to eq 20
@@ -40,5 +52,15 @@ describe Array do
     expect([1, 4, 5].my_inject([]) do |array, num|
       array << num * 2
     end).to eq [2, 8, 10]
+  end
+  it 'can find the longest word' do
+    longest = %w( cat sheep bear ).inject do |memo, word|
+      memo.length > word.length ? memo : word
+    end
+    expect(longest).to eq 'sheep'
+    longest = %w( cat sheep bear ).my_inject do |memo, word|
+      memo.length > word.length ? memo : word
+    end
+    expect(longest).to eq 'sheep'
   end
 end
