4f78bb22be280422b6a693f2a6a4efab127f7762
paulwallis42 <paulwallis42@gmail.com>
2015-03-28 16:00:28 +0000

    first commit

---
+++ lib/inject.rb
@@ -0,0 +1,24 @@
+class Array
+  def my_inject(start=0, symbol=0, &block)
+    multiply = 1
+    add = 0
+
+
+
+
+# 'Can add'                   =>    self.map { |i| add += i }.last
+# 'Can add with start point'  =>    self.map { |i| start + add += i }.last
+# 'Can multiply'              =>    self.map { |i| multiply *=i }.last
+# 'Can mult with start pt'    =>    self.map { |i| start * multiply *=i }.last
+# 'Can add with a symbol'     =>    self.map { |i| add += i }.last
+# 'Can add sym & start'       =>    self.map { |i| start + add += i }.last
+# 'Can find longest stringse  =>    self.sort_by { |x| x.length }.last
+  end
+end
+
+
+
+
+
+
+#(start=0, symbol=0, block)
