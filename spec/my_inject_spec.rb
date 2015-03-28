bdc4a3a806978f1ad4dbdd5164e42ccf5776b938
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-28 14:02:21 +0000

    it can substract

--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -11,6 +11,11 @@ describe Array do
     expect([7, 8, 9].my_inject { |memo, element| memo * element }).to eq 504
   end
 
+  it 'it can substract' do
+    expect([1, 2, 3].inject { |memo, element| memo - element }).to eq -4
+    expect([7, 2, 6].my_inject { |memo, element| memo - element }).to eq -1
+  end
+
   it 'can add with a starting point' do
     expect([1, 2, 3].inject(10) { |memo, element| memo + element }).to eq 16
     expect([2, 9, 3].my_inject(10) { |memo, element| memo + element }).to eq 24
@@ -19,6 +24,11 @@ describe Array do
   it 'can times with a starting point' do
     expect([1, 2, 3].inject(10) { |memo, element| memo * element }).to eq 60
     expect([3, 7, 4].my_inject(11) { |memo, element| memo * element }).to eq 924
+  end
+
+  xit 'it can substract with a starting point' do
+    expect([1, 2, 3].inject(-6) { |memo, element| memo - element }).to eq -12
+    expect([7, 2, 6].my_inject(-7) { |memo, element| memo - element }).to eq -22
   end
 
   it 'can add with a symbol' do
@@ -30,4 +40,6 @@ describe Array do
     expect([1, 2, 3].inject(:*)).to eq 6
     expect([6, 7, 2].my_inject(:*)).to eq 84
   end
+
+
 end
