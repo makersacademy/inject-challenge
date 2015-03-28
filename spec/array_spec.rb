1e114f09573729a7a0b8681025f79a7e12413bfa
Jade King <dev@jadeking.co.uk>
2015-03-28 20:57:26 +0000

    fixed array test

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,7 +1,7 @@
 require 'array'
 describe Array do
   it 'calculate sum of array' do
-    result = [1, 2, 3].my_inject(0) { |num, sum| sum += num }
+    result = [1, 2, 3].my_inject(0) { |num, sum| num + sum }
     expect(result).to eq 6
   end
 end
