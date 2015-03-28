6a417ca7d8beacacebb8718d213175dd0effb64e
Mark Pokorny <iridium193@gmail.com>
2015-03-28 22:03:44 +0000

    Updated ‘custom_inject’ method to respond to symbol parameters.

--- lib/array.rb
+++ lib/array.rb
@@ -1,7 +1,18 @@
 class Array
-  def custom_inject
+  def custom_inject(operation = nil)
     injector = shift # i.e: self.shift (RuboCop is not being friendly!)
-    each { |num| injector = yield(injector, num) }
+    case operation
+    when :+
+      each { |num| injector += num }
+    when :-
+      each { |num| injector -= num }
+    when :*
+      each { |num| injector *= num }
+    when :/
+      each { |num| injector /= num }
+    else
+      each { |num| injector = yield(injector, num) } if block_given?
+    end
     injector
   end
 end
