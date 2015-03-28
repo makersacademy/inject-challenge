5dd82ae30aa479c974e0f1229bae50d49a0a04f5
Jade King <dev@jadeking.co.uk>
2015-03-28 21:59:09 +0000

    update my_inject for multiply test

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,8 @@
 class Array
-  def my_inject(start)
-    sum = start
+  def my_inject(start = nil)
+    start.nil? ? sum = first && shift : sum = start
+    # sum = start
+    # shift if start.nil?
     each { |num| sum = yield(sum, num) }
     sum
   end
