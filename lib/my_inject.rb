86668781ab8587b4fef8073a469282f69393e3b6
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-28 13:27:08 +0000

    it can add with a symbol

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,13 +1,19 @@
 class Array
   def my_inject starting = 0, *_or_symbol, &_or_block
+    if (starting == :+ )
+    _or_block = Proc.new{ |memo, element| memo + element }
+    p _or_block
+     # undefined method call for nilclass
+    end
+
     accumulator = self[0]
     arr = drop(1)
     arr.each do |element|
-      accumulator = yield(accumulator, element)
+      accumulator = _or_block.call(accumulator, element)
     end
 
-    if starting != 0
-      accumulator = yield(accumulator, starting)
+    if starting != 0 && starting != :+
+      accumulator = _or_block.call(accumulator, starting)
     else
       accumulator
     end
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -3,27 +3,27 @@ require 'my_inject'
 describe Array do
   it 'it can add' do
     expect([1, 2, 3].inject { |memo, element| memo + element }).to eq 6
-    expect([1, 2, 3].my_inject { |memo, element| memo + element }).to eq 6
+    expect([7, 2, 6].my_inject { |memo, element| memo + element }).to eq 15
   end
 
   it 'can times' do
     expect([1, 2, 3].inject { |memo, element| memo * element }).to eq 6
-    expect([1, 2, 3].my_inject { |memo, element| memo * element }).to eq 6
+    expect([7, 8, 9].my_inject { |memo, element| memo * element }).to eq 504
   end
 
   it 'can add with a starting point' do
     expect([1, 2, 3].inject(10) { |memo, element| memo + element }).to eq 16
-    expect([1, 2, 3].my_inject(10) { |memo, element| memo + element }).to eq 16
+    expect([2, 9, 3].my_inject(10) { |memo, element| memo + element }).to eq 24
   end
 
   it 'can times with a starting point' do
     expect([1, 2, 3].inject(10) { |memo, element| memo * element }).to eq 60
-    expect([1, 2, 3].my_inject(10) { |memo, element| memo * element }).to eq 60
+    expect([3, 7, 4].my_inject(11) { |memo, element| memo * element }).to eq 924
   end
 
   it 'can add with a symbol' do
     expect([1, 2, 3].inject(:+)).to eq 6
-    # expect([1, 2, 3].my_inject(:+)).to eq 6
+    expect([4, 7, 3, 5, 3].my_inject(:+)).to eq 22
   end
 
   it 'can times with a symbol' do
