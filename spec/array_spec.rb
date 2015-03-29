377aa3663f154dd2fb7c0b67a199f762987a2749
Jade King <dev@jadeking.co.uk>
2015-03-29 02:22:21 +0100

    corrected test with symbol *

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -8,12 +8,28 @@ describe Array do
     expect([1, 2, 3].my_inject(2) { |num, sum| num + sum }).to eq 8
     expect([1, 2, 3].inject(2) { |num, sum| num + sum }).to eq 8
   end
-  it 'multiplies array values together' do
+  it 'multiplies ' do
     expect([2, 2, 4].inject { |num, sum| num * sum }).to eq 16
     expect([2, 2, 4].my_inject { |num, sum| num * sum }).to eq 16
   end
-  it 'multiplies array values together with argument' do
+  it 'multiplies array values together with initial number arg' do
     expect([2, 2, 4].inject(2) { |num, sum| num * sum }).to eq 32
     expect([2, 2, 4].my_inject(2) { |num, sum| num * sum }).to eq 32
   end
+  it 'calculate sum of array with symbol +' do
+    expect([1, 2, 3].my_inject(:+)).to eq 6
+    expect([1, 2, 3].inject(:+)).to eq 6
+  end
+  it 'calculate sum of array with symbol *' do
+    expect([2, 2, 4].my_inject(:*)).to eq 16
+    expect([2, 2, 4].inject(:*)).to eq 16
+  end
+  # it 'calculate sum of array with initial number arg and symbol + ' do
+  #   expect([1, 2, 3].my_inject(2, :+)).to eq 8
+  #   expect([1, 2, 3].inject(2, :+)).to eq 8
+  # end
+  # it 'multiplies with initial number arg and symbol *' do
+  #   expect([1, 2, 3].my_inject(2, :*)).to eq 12
+  #   expect([1, 2, 3].inject(2, :*)).to eq 12
+  # end
 end
