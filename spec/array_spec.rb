0e69d2c071e4abc3467bfd0d79cfeea198fa5408
Jade King <dev@jadeking.co.uk>
2015-03-29 02:43:53 +0100

    fixed test

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -16,14 +16,14 @@ describe Array do
     expect([2, 2, 4].inject(2) { |num, sum| num * sum }).to eq 32
     expect([2, 2, 4].my_inject(2) { |num, sum| num * sum }).to eq 32
   end
-  # it 'calculate division from 0' do
-  #   expect([1, 2, 3].my_inject(0) { |num, sum| num / sum }).to eq 0
-  #   expect([1, 2, 3].inject(0) { |num, sum| num / sum }).to eq 0
-  # end
-  # it 'calculate division' do
-  #   expect([50, 2, 2].my_inject { |num, sum| num / sum }).to eq 12
-  #   expect([50, 2, 2].inject { |num, sum| num / sum }).to eq 12
-  # end
+  it 'calculate division from 0' do
+    expect([1, 2, 3].my_inject(0) { |num, sum| num / sum }).to eq 0
+    expect([1, 2, 3].inject(0) { |num, sum| num / sum }).to eq 0
+  end
+  it 'calculate division' do
+    expect([50, 2, 2].my_inject { |num, sum| num / sum }).to eq 12
+    expect([50, 2, 2].inject { |num, sum| num / sum }).to eq 12
+  end
   it 'calculate sum of array with symbol +' do
     expect([1, 2, 3].my_inject(:+)).to eq 6
     expect([1, 2, 3].inject(:+)).to eq 6
