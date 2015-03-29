0eb498eb298b4fb7eead0c85d4f861f8f0301700
Tomiblanchard <tomiblanchard@gmail.com>
2015-03-29 00:19:50 +0000

    refactored

--- lib/array.rb
+++ lib/array.rb
@@ -1,54 +1,23 @@
 class Array
-  #def my_inject(memo = nil)
-  #  memo = nil || self.slice!(0)
-      # the slice!() method Deletes the element(s) given by an index
-      # (optionally up to length elements) or by a range.
-  #    self.each do |element|
-  #    memo = yield memo, element
-  #    end
-  #  end
-  #  p memo
-  #end
-#end
-
-# self, yield, shift, each, block, slice
-
-#def my_inject(starting_point = nil)
-#		if starting_point
-#			memo = starting_point #it's memory starts with the starting_point argument
-#		else
-#			memo = self.shift #this removes the first from the array given and assigns it to memory
-#		end
-
-#		self.each do |item| # it iterates through the new array 1 by 1
-#			memo = yield memo, item
-#		end
-#		memo
-#end
-
-  def my_inject(initial=nil, symbol=nil)
-
-    if initial.class == Symbol
-      return self.my_inject {|memory,element| memory.send(initial,element )}
-    end
-     # .send() invokes the method identified by symbol,
-     # passing it any arguments specified.
-    if symbol.class == Symbol
-      return self.my_inject(initial) {|memory,element| memory.send(symbol, element)}
-    end
-
-    if initial != nil
-      self.unshift(initial)
+  def my_inject(initial = nil, symbol = nil)
+    if symbol.kind_of? Symbol
+      return my_inject(initial) {|memory, element| memory.send(symbol, element)}
+      # .send() invokes the method identified by symbol,
+      # passing it any arguments specified.
+    elsif !initial.nil?
+      unshift(initial)
       # .unshift() adds one or more elements to the beginning
       # of an array and returns the new length of the array.
     end
 
     memory = self[0]
-    self[1..-1].each { |item| memory = yield memory, item }
-      # .each {} calls the given block once for each element in self,
-      # passing that element as a parameter.
 
+    self[1..-1].each do |element|
+      # .each calls the given block once for each element in self,
+      # passing that element as a parameter.
+      memory = yield memory, element
       # yield runs the given block passing it the memory and the items 1 by 1
-    memory
+    end
+    p memory
   end
 end
