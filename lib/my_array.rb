9d576afcafb92198ee09214ab196f2fa9bf5ba0d
Richard Lynch <rjlynch86@googlemail.com>
2015-03-29 10:29:17 +0100

    refactored to please rubcop

--- lib/my_array.rb
+++ lib/my_array.rb
@@ -1,21 +1,35 @@
-
 class Array
   def my_inject arg = false, arg_sym = false, &block
-    arr = self
-
-    if arg_sym
-      total = arg
-      arr.each { |el| total =  total.method(arg_sym).call(el) }
+    @arr = self
+    what_are_my_arguments arg, arg_sym
+    total = @start
+    if @sym
+      @arr.each { |el| total =  total.method(@sym).call(el) }
     else
-      case arg
-      when Symbol
-        total = arr.shift
-        arr.each { |el| total =  total.method(arg).call(el) }
-      else
-        arg ? total = arg : total = arr.shift
-        arr.each { |el| total = block.call(total, el) }
-      end
+      @arr.each { |el| total = block.call(total, el) }
     end
     total
+  end
+
+  def what_are_my_arguments arg, arg_sym
+    if arg_sym.is_a? Symbol
+      i_has_two arg, arg_sym
+    else
+      if arg.is_a? Symbol
+        i_has_just_a_symbol arg
+      else
+        arg ? @start = arg : @start = @arr.shift
+      end
+    end
+  end
+
+  def i_has_two arg, arg_sym
+    @start = arg
+    @sym = arg_sym
+  end
+
+  def i_has_just_a_symbol arg
+    @sym = arg
+    @start = @arr.shift
   end
 end
