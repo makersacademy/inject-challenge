3c92e273783e45cfa558a9d9913e1d9d815cde34
paulwallis42 <paulwallis42@gmail.com>
2015-03-29 16:09:30 +0100

    second

--- lib/inject.rb
+++ lib/inject.rb
@@ -3,16 +3,13 @@ class Array
     multiply = 1
     add = 0
 
-
-
-
-# 'Can add'                   =>    self.map { |i| add += i }.last
-# 'Can add with start point'  =>    self.map { |i| start + add += i }.last
-# 'Can multiply'              =>    self.map { |i| multiply *=i }.last
-# 'Can mult with start pt'    =>    self.map { |i| start * multiply *=i }.last
-# 'Can add with a symbol'     =>    self.map { |i| add += i }.last
-# 'Can add sym & start'       =>    self.map { |i| start + add += i }.last
-# 'Can find longest stringse  =>    self.sort_by { |x| x.length }.last
+'Can add'                   =>    self.map { |i| add += i }.last
+'Can add with start point'  =>    self.map { |i| start + add += i }.last
+'Can multiply'              =>    self.map { |i| multiply *=i }.last
+'Can mult with start pt'    =>    self.map { |i| start * multiply *=i }.last
+'Can add with a symbol'     =>    self.map { |i| add += i }.last
+'Can add sym & start'       =>    self.map { |i| start + add += i }.last
+'Can find longest strings'  =>    self.sort_by { |x| x.length }.last
   end
 end
 
