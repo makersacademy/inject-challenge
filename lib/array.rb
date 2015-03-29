eb0ee6f8a76681b7532cfcbe1db9776c46e867f9
icicleta <icicleta@gmail.com>
2015-03-29 14:44:32 +0100

    fixed the problem of starting being or not being a symbol but still getting 7 instead of 6 in sum

--- lib/array.rb
+++ lib/array.rb
@@ -1,12 +1,11 @@
 class Array
-  def my_inject(starting = nil, symbol = nil)
+  def my_inject starting = nil, symbol = nil
     starting.nil? ? result = self[0] : result = starting
-    if !symbol.nil?
-      if starting.nil?
-        each { |num| result = result.send symbol, num }
-      else
-        each { |num| result = result.send symbol, num }
-      end
+    if starting.is_a? Symbol
+      result = self[0]
+      each { |num| result = result.send starting, num }
+    elsif symbol.is_a? Symbol
+      each { |num| result = result.send symbol, num }
     else
       each { |num| result = yield result, num } if block_given?
     end
@@ -17,5 +16,5 @@ end
 p [1, 2, 3].my_inject { |result, number| result + number }
 p [1, 2, 3].my_inject { |result, number| result * number }
 p [1, 2, 3].my_inject(10) { |result, number| result + number }
-p [1, 2, 3].my_inject(0, :+)
-p [1, 2, 3].my_inject(:*) # 1.+(2)
+p [1, 2, 3].my_inject(10, :*)
+p [1, 2, 3].my_inject(:+) # 1.+(2)
