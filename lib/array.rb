66cee5af64aac6b0849541c4a37b913cd23630bd
Mark Pokorny <iridium193@gmail.com>
2015-03-29 12:42:43 +0100

    Updated ‘custom_inject’ to take an initial argument. Added extra methods to extract sumbol/initial value from parameters and to apply required operations.

--- lib/array.rb
+++ lib/array.rb
@@ -1,17 +1,25 @@
 class Array
-  def custom_inject(operation = nil)
-    injector = shift # i.e: self.shift (RuboCop is not being friendly!)
+  def custom_inject(*args, &block)
+    operation, injector = retrieve_args(*args)
+    injector ||= shift # i.e: self.shift (RuboCop is not being friendly!)
+    operation_select(operation, injector, block)
+  end
+
+  private
+
+  def retrieve_args(*args)
+    symbol = args.select { |arg| arg.is_a?(Symbol) }.first
+    initial_value = args.select { |arg| arg.is_a?(Numeric) }.first
+    [symbol, initial_value]
+  end
+
+  def operation_select(operation, injector, block)
     case operation
-    when :+
-      each { |num| injector += num }
-    when :-
-      each { |num| injector -= num }
-    when :*
-      each { |num| injector *= num }
-    when :/
-      each { |num| injector /= num }
-    else
-      each { |num| injector = yield(injector, num) } if block_given?
+    when :+ then each { |num| injector += num }
+    when :- then each { |num| injector -= num }
+    when :* then each { |num| injector *= num }
+    when :/ then each { |num| injector /= num }
+    else each { |num| injector = block.call(injector, num) }
     end
     injector
   end
