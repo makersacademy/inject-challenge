50ee7a209f8d7269a5f2775d7394d9d424ea93a1
Jade King <dev@jadeking.co.uk>
2015-03-29 02:33:15 +0100

    test sum of array with initialiser & symbol

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -16,6 +16,14 @@ describe Array do
     expect([2, 2, 4].inject(2) { |num, sum| num * sum }).to eq 32
     expect([2, 2, 4].my_inject(2) { |num, sum| num * sum }).to eq 32
   end
+  # it 'calculate division from 0' do
+  #   expect([1, 2, 3].my_inject(0) { |num, sum| num / sum }).to eq 0
+  #   expect([1, 2, 3].inject(0) { |num, sum| num / sum }).to eq 0
+  # end
+  # it 'calculate division' do
+  #   expect([50, 2, 2].my_inject { |num, sum| num / sum }).to eq 12
+  #   expect([50, 2, 2].inject { |num, sum| num / sum }).to eq 12
+  # end
   it 'calculate sum of array with symbol +' do
     expect([1, 2, 3].my_inject(:+)).to eq 6
     expect([1, 2, 3].inject(:+)).to eq 6
@@ -24,10 +32,10 @@ describe Array do
     expect([2, 2, 4].my_inject(:*)).to eq 16
     expect([2, 2, 4].inject(:*)).to eq 16
   end
-  # it 'calculate sum of array with initial number arg and symbol + ' do
-  #   expect([1, 2, 3].my_inject(2, :+)).to eq 8
-  #   expect([1, 2, 3].inject(2, :+)).to eq 8
-  # end
+  it 'calculate sum of array with initial number arg and symbol + ' do
+    expect([1, 2, 3].my_inject(2, :+)).to eq 8
+    expect([1, 2, 3].inject(2, :+)).to eq 8
+  end
   # it 'multiplies with initial number arg and symbol *' do
   #   expect([1, 2, 3].my_inject(2, :*)).to eq 12
   #   expect([1, 2, 3].inject(2, :*)).to eq 12
