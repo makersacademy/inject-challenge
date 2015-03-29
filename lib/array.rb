6f1bca7602496999b7abf3010f082e454f744583
icicleta <icicleta@gmail.com>
2015-03-29 00:06:10 +0000

    add block_given to if!symbol.nil? as else, instead of being out wiht if statement

--- lib/array.rb
+++ lib/array.rb
@@ -1,11 +1,14 @@
 class Array
   def my_inject(starting = nil, symbol = nil)
     starting.nil? ? result = self[0] : result = starting
-    each { |num| result = yield result, num } if block_given?
     if !symbol.nil?
-      each { |num| result = result.send symbol, num }
-    elsif !symbol.nil? && starting.nil?
-      each { |num| result = result.send symbol, num }
+      if starting.nil?
+        each { |num| result = result.send symbol, num }
+      else
+        each { |num| result = result.send symbol, num }
+      end
+    else
+      each { |num| result = yield result, num } if block_given?
     end
     result
   end
@@ -16,4 +19,3 @@ p [1, 2, 3].my_inject { |result, number| result * number }
 p [1, 2, 3].my_inject(10) { |result, number| result + number }
 p [1, 2, 3].my_inject(0, :+)
 p [1, 2, 3].my_inject(:*) # 1.+(2)
-
