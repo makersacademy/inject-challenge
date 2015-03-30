febc0b347f7e4702e01bf2072b9b71d0dc9409d0
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-30 09:20:48 +0100

    Improved tests to check against actual Inject method

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,24 +3,30 @@ require 'array'
 describe Array do
   context '#gusject method' do
     it 'sums all the members of the array' do
-      expect([2, 3, 4].gusject { |sum, el| sum + el }).to eq 9
+      expect([2, 3, 4].gusject do |sum, el|
+        sum + el
+      end).to eq([2, 3, 4].inject { |sum, el| sum + el })
     end
 
     it 'multiplies all the members of the array' do
-      expect([2, 3, 4].gusject { |sum, el| sum * el }).to eq 24
+      expect([2, 3, 4].gusject do |sum, el|
+        sum * el
+      end).to eq([2, 3, 4].inject { |sum, el| sum * el })
     end
 
     it 'allows for the initial value to be set' do
-      expect([2, 3, 4].gusject(10) { |sum, el| sum + el }).to eq 19
+      expect([2, 3, 4].gusject(10) do |sum, el|
+        sum + el
+      end).to eq([2, 3, 4].inject(10) { |sum, el| sum + el })
     end
 
     it 'accepts symbols instead of a block' do
-      expect([2, 3, 4].gusject(:+)).to eq 9
-      expect([2, 3, 4].gusject(:*)).to eq 24
+      expect([2, 3, 4].gusject(:+)).to eq([2, 3, 4].inject(:+))
+      expect([2, 3, 4].gusject(:*)).to eq([2, 3, 4].inject(:*))
     end
 
     it 'can set initial value with symbols' do
-      expect([2, 3, 4].gusject(10, :+)).to eq 19
+      expect([2, 3, 4].gusject(10, :+)).to eq([2, 3, 4].inject(10, :+))
     end
   end
 end
