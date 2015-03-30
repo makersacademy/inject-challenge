c641f345f852e9472496117720d4863eb9fc632b
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-30 09:14:00 +0100

    Refactored to me more OO

--- lib/array.rb
+++ lib/array.rb
@@ -1,22 +1,30 @@
 class Array
   def gusject(*args, &block)
-    args << block unless block.nil?
-    case args.length
-    when 2
-      sum = args[0]
-      if block_given?
-        each { |el| sum = block.call(sum, el) }
-      else
-        each { |el| sum = sum.send(args[1], el) }
-      end
-    when 1
-      temp_array = dup
-      sum = temp_array.shift
-      if block_given?
-        temp_array.each { |el| sum = block.call(sum, el) }
-      else
-        temp_array.each { |el| sum = sum.send(args[0], el) }
-      end
+    if args.empty? || args[0].instance_of?(Symbol)
+      gusject_no_initial(args, block)
+    else
+      gusject_with_initial(args, block)
+    end
+  end
+
+  private
+
+  def gusject_with_initial(args, block)
+    sum = args[0]
+    if block
+      each { |el| sum = block.call(sum, el) }
+    else
+      each { |el| sum = sum.send(args[1], el) }
+    end
+    sum
+  end
+
+  def gusject_no_initial(args, block)
+    sum = first
+    if block
+      (1...length).each { |index| sum = block.call(sum, self[index]) }
+    else
+      (1...length).each { |index| sum = sum.send(args[0], self[index]) }
     end
     sum
   end
