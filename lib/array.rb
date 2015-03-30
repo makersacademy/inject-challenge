a5d066fa132578e7cbb21a5a8e6af3b672770558
sanjsanj <sanjsanj@hotmail.com>
2015-03-30 08:53:12 +0100

    finally solved! Rubocop pass

--- lib/array.rb
+++ lib/array.rb
@@ -3,13 +3,11 @@ class Array
     arg.nil? || arg.is_a?(Symbol) ? result = shift : result = arg
     if arg.is_a?(Symbol)
       each { |value| result = result.send(arg, value) }
-      result
     elsif arg && arg_sym
       each { |value| result = result.send(arg_sym, value) }
-      result
     else
       each { |value| result = yield(result, value) }
-      result
     end
+    result
   end
 end
