707bd31fa229c57bbffd1619ee679865e10b33c7
icicleta <icicleta@gmail.com>
2015-03-30 20:14:34 +0100

    restructuration of class

--- lib/array.rb
+++ lib/array.rb
@@ -1,15 +1,25 @@
 class Array
   def my_inject starting = nil, symbol = nil
-    starting.nil? ? result = self[0] : result = starting
-    if starting.is_a? Symbol
-      result = self[0]
-      each { |num| result = result.send starting, num }
-    elsif symbol.is_a? Symbol
-      each { |num| result = result.send symbol, num }
-    else
-      each { |num| result = yield result, num }
-    end
-    result
+    result = starting.nil? ? self[0] : starting
+    return method_with_starting starting if starting.is_a? Symbol
+    return method_with_symbol if symbol.is_a? Symbol
+    method_with_block
+  end
+
+  private
+
+  attr_reader :result, :symbol
+
+  def method_with_starting
+    result = self[0]
+    drop(0).each { |num| result = result.send starting, num }
+  end
+
+  def method_with_symbol
+    drop(0).each { |num| result = result.send symbol, num }
+  end
+
+  def method_with_block
+    drop(0).each { |num| result = yield result, num }
   end
 end
-
