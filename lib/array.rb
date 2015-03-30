4474503c1a9ad5bc47fce31286ee337b1eb0fce0
sanjsanj <sanjsanj@hotmail.com>
2015-03-30 07:59:29 +0100

    remove comments

--- lib/array.rb
+++ lib/array.rb
@@ -1,18 +1,18 @@
 class Array
   def my_inject(arg = nil, arg_sym = nil)
     arg.nil? || arg.is_a?(Symbol) ? result = self[0] : result = arg
-    if arg.nil? # inject without any args
+    if arg.nil?
       self[1..-1].each { |value| result = yield(result, value) }
       result
     else
-      if arg.is_a?(Symbol) # inject with symbol
+      if arg.is_a?(Symbol)
         self[1..-1].each { |value| result = result.send(arg, value) }
         result
       else
-        if arg && arg_sym # inject with arg and symbol
+        if arg && arg_sym
           each { |value| result = result.send(arg_sym, value) }
           result
-        else # inject with arg starting point
+        else
           each { |value| result = yield(result, value) }
           result
         end
