b2cf8951b63bad07a28ffc7b86c73bde0aae7c3f
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-30 09:45:20 +0100

    Added extensive testing of most common math operations

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,11 +1,17 @@
 require 'array'
 
 describe Array do
-  context '#gusject method' do
+  context '#gusject method uses blocks' do
     it 'sums all the members of the array' do
       expect([2, 3, 4].gusject do |sum, el|
         sum + el
       end).to eq([2, 3, 4].inject { |sum, el| sum + el })
+    end
+
+    it 'subtracts all the members of the array from first' do
+      expect([2, 3, 4].gusject do |sum, el|
+        sum - el
+      end).to eq([2, 3, 4].inject { |sum, el| sum - el })
     end
 
     it 'multiplies all the members of the array' do
@@ -14,19 +20,52 @@ describe Array do
       end).to eq([2, 3, 4].inject { |sum, el| sum * el })
     end
 
-    it 'allows for the initial value to be set' do
+    it 'divides first member by all others' do
+      expect([100, 3, 4].gusject do |sum, el|
+        sum / el
+      end).to eq([100, 3, 4].inject { |sum, el| sum / el })
+    end
+  end
+
+  context '#gusject method allows initial value to be set' do
+    it 'sums' do
       expect([2, 3, 4].gusject(10) do |sum, el|
         sum + el
       end).to eq([2, 3, 4].inject(10) { |sum, el| sum + el })
     end
 
-    it 'accepts symbols instead of a block' do
-      expect([2, 3, 4].gusject(:+)).to eq([2, 3, 4].inject(:+))
-      expect([2, 3, 4].gusject(:*)).to eq([2, 3, 4].inject(:*))
+    it 'subtracts' do
+      expect([2, 3, 4].gusject(10) do |sum, el|
+        sum - el
+      end).to eq([2, 3, 4].inject(10) { |sum, el| sum - el })
     end
 
-    it 'can set initial value with symbols' do
+    it 'multiplies' do
+      expect([2, 3, 4].gusject(10) do |sum, el|
+        sum * el
+      end).to eq([2, 3, 4].inject(10) { |sum, el| sum * el })
+    end
+
+    it 'divides' do
+      expect([2, 3, 4].gusject(10) do |sum, el|
+        sum / el
+      end).to eq([2, 3, 4].inject(10) { |sum, el| sum / el })
+    end
+  end
+
+  context '#gusject method uses symbols' do
+    it 'accepts symbols instead of a block' do
+      expect([2, 3, 4].gusject(:+)).to eq([2, 3, 4].inject(:+))
+      expect([2, 3, 4].gusject(:-)).to eq([2, 3, 4].inject(:-))
+      expect([2, 3, 4].gusject(:*)).to eq([2, 3, 4].inject(:*))
+      expect([2, 3, 4].gusject(:/)).to eq([2, 3, 4].inject(:/))
+    end
+
+    it 'can set initial values and uses symbols' do
       expect([2, 3, 4].gusject(10, :+)).to eq([2, 3, 4].inject(10, :+))
+      expect([2, 3, 4].gusject(10, :-)).to eq([2, 3, 4].inject(10, :-))
+      expect([2, 3, 4].gusject(10, :*)).to eq([2, 3, 4].inject(10, :*))
+      expect([2, 3, 4].gusject(10, :/)).to eq([2, 3, 4].inject(10, :/))
     end
   end
 end
