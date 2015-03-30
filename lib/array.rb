1719ebe04318d1984cbafe5faadb6912d20f0435
sanjsanj <sanjsanj@hotmail.com>
2015-03-30 08:49:42 +0100

    refactor, only 1 rubocop error

--- lib/array.rb
+++ lib/array.rb
@@ -1,22 +1,15 @@
 class Array
   def my_inject(arg = nil, arg_sym = nil)
-    arg.nil? || arg.is_a?(Symbol) ? result = self[0] : result = arg
-    if arg.nil?
-      self[1..-1].each { |value| result = yield(result, value) }
+    arg.nil? || arg.is_a?(Symbol) ? result = shift : result = arg
+    if arg.is_a?(Symbol)
+      each { |value| result = result.send(arg, value) }
+      result
+    elsif arg && arg_sym
+      each { |value| result = result.send(arg_sym, value) }
       result
     else
-      if arg.is_a?(Symbol)
-        self[1..-1].each { |value| result = result.send(arg, value) }
-        result
-      else
-        if arg && arg_sym
-          each { |value| result = result.send(arg_sym, value) }
-          result
-        else
-          each { |value| result = yield(result, value) }
-          result
-        end
-      end
+      each { |value| result = yield(result, value) }
+      result
     end
   end
 end
