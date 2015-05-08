97cb8bcb983ca35659c482ec41aaf332ede7928e
Ashleigh <ashleighmaund@gmail.com>
2015-05-08 15:27:54 +0100

    passed first test - input responds to method test

---
+++ lib/ashleighs_rubbish_inject.rb
@@ -0,0 +1,6 @@
+class Array
+  def inject_method
+  	"SOME OUTPUT"
+  end
+
+end
--- spec/ashleighs_rubbish_inject_spec.rb
+++ spec/ashleighs_rubbish_inject_spec.rb
@@ -1,5 +1,9 @@
 require 'ashleighs_rubbish_inject'
 
 describe Array do
+  it 'to receive array as input and give output' do
+    expect(subject).to respond_to(:inject_method)
+    expect(subject.inject_method).not_to eq nil
+  end
   	
 end
