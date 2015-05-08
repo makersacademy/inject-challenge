b9014c377555003b0162f9f2e8dea06de237b938
Ashleigh <ashleighmaund@gmail.com>
2015-05-08 15:32:35 +0100

    amended first test by splitting into two - passed both

--- spec/ashleighs_rubbish_inject_spec.rb
+++ spec/ashleighs_rubbish_inject_spec.rb
@@ -1,8 +1,12 @@
 require 'ashleighs_rubbish_inject'
 
 describe Array do
-  it 'to receive array as input and give output' do
+
+  it 'to respond to the new inject method' do
     expect(subject).to respond_to(:inject_method)
+  end
+
+  it 'to give an output' do
     expect(subject.inject_method).not_to eq nil
   end
   	
