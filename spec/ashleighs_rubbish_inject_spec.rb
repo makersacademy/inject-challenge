b275fba3bae482711be7fdb8e08be4c718c937d9
Ashleigh <ashleighmaund@gmail.com>
2015-05-08 15:36:46 +0100

    passed third test - to receive two arguments

--- spec/ashleighs_rubbish_inject_spec.rb
+++ spec/ashleighs_rubbish_inject_spec.rb
@@ -7,10 +7,10 @@ describe Array do
   end
 
   it 'to give an output' do
-    expect(subject.inject_method).not_to eq nil
+    expect(subject.inject_method(:input,:operator)).not_to eq nil
   end
 
-  it 'to receive object and operator as arguments' do
+  it 'to receive two arguments' do
   	expect(subject).to respond_to(:inject_method).with(2).arguments
   end
   	
