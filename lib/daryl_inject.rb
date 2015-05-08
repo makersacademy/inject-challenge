83f6cb64d083f52470265d267051c42abed44f12
Daryl Watson <darylwtsn@gmail.com>
2015-05-08 16:35:01 +0100

    Refactored and added more tests

--- lib/daryl_inject.rb
+++ lib/daryl_inject.rb
@@ -2,15 +2,9 @@ class Array
 
   def daryl_inject(default = self.first, &block)
 
-  sum = default
-  if sum == self.first
-    self.shift
-  end
-  self.each do |x|
-    sum = block.call(sum, x)
-  end
-
-  sum
+    self.shift if default == self.first
+    self.each { |x| default = block.call(default, x) }
+    default
 
   end
 end
--- spec/daryl_inject_spec.rb
+++ spec/daryl_inject_spec.rb
@@ -4,26 +4,38 @@ describe Array do
 
   it 'adds up an array of numbers' do
     output = (1..10).to_a.daryl_inject(0) { |sum, number| sum + number }
-    p output
     expect(output).to eq ((1..10)).inject(0) { |sum, number| sum + number }
   end
 
   it 'adds up an array of numbers without default value' do
     output = (1..10).to_a.daryl_inject { |sum, number| sum + number }
-    p output
     expect(output).to eq ((1..10)).inject { |sum, number| sum + number }
   end
 
   it 'adds up an array of numbers without default value' do
     output = (1..4).to_a.daryl_inject { |sum, number| sum + number }
-    p output
     expect(output).to eq ((1..4)).inject { |sum, number| sum + number }
   end
 
   it 'subtracts up an array of numbers' do
     output = (1..10).to_a.daryl_inject(0) { |sum, number| sum - number }
-    p output
     expect(output).to eq ((1..10)).inject(0) { |sum, number| sum - number }
   end
+
+  it 'multiplies up an array of numbers' do
+    output = (1..50).to_a.daryl_inject(1) { |sum, number| sum * number }
+    expect(output).to eq ((1..50)).inject(1) { |sum, number| sum * number }
+  end
+
+  it 'multiplies up an array of numbers without a default' do
+    output = (1..50).to_a.daryl_inject { |sum, number| sum * number }
+    expect(output).to eq ((1..50)).inject { |sum, number| sum * number }
+  end
+
+  it 'can handle strings' do
+    output = ['Daryl, Tim, Ptolemy, Ben, Sam'].daryl_inject { |sum, firstword| sum + firstword }
+    expect(output).to eq ['Daryl, Tim, Ptolemy, Ben, Sam'].inject { |sum, firstword| sum + firstword }
+  end
+
 
 end
