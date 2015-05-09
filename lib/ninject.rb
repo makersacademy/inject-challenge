d8a9663dd3e8a7d948f2e794de9a2ea6a1b02ee3
Rodney Cullen <rodcul@gmail.com>
2015-05-09 01:36:14 +0100

    Monkeypatch - inject(initial) { |memo, obj| block } → obj

---
+++ lib/ninject.rb
@@ -0,0 +1,13 @@
+class Array
+  def ninject(start = nil, &code)
+    proc = Proc.new &code
+    each_with_index do |obj, index|
+      if index == 0
+        start.nil? ? @memo = obj : @memo = proc.call(start, obj)
+      else
+        @memo = proc.call(@memo, obj)
+      end
+    end
+    @memo
+  end
+end
