c6d0753b9fa2add23a84e3f887c4fcdf54b37894
Ashleigh <ashleighmaund@gmail.com>
2015-05-09 18:05:21 +0100

    amended arguments being accepted by new method, so changed two tests. passing - for now

--- lib/ashleighs_rubbish_inject.rb
+++ lib/ashleighs_rubbish_inject.rb
@@ -1,6 +1,10 @@
 class Array
-  def inject_method(some_input, operator)
-  	"SOME OUTPUT"
+  def inject_method(arg)
+  	arg
   end
-
 end
+
+
+#   	# need to use yield to get pull the block from
+#   	# up there
+# # use proc? ummm block and yield
--- spec/ashleighs_rubbish_inject_spec.rb
+++ spec/ashleighs_rubbish_inject_spec.rb
@@ -7,11 +7,37 @@ describe Array do
   end
 
   it 'to give an output' do
-    expect(subject.inject_method(:input,:operator)).not_to eq nil
+    expect(subject.inject_method(:code)).not_to eq nil
   end
 
   it 'to receive two arguments' do
-  	expect(subject).to respond_to(:inject_method).with(2).arguments
+  	expect(subject).to respond_to(:inject_method).with(1).arguments
   end
+
+# how to pass operator to our method? Googlings
+# => need to write operator as a symbol like ":+"?
+# not doing this now, too complicated for my tiny brain :P
+# decided to persevere with using block
+
+# need to figure out if there is a test for this?
+# does inject method only accept blocks?
+# ... yesss it does. Can I test for that?
+  xit 'to receive block as argument' do
+  end
+
+  xit 'to receive number as an argument and output number or boolean' do
+  end
+
+  xit 'to receive string as an argument and output string' do
+  end
+
+  context 'to return the same values as inject method:' do
+    xit 'array = [1,2,3,4], input = 0 and operator = \'+\', return true' do
+      array = [1,2,3,4]
+	  new_method = array.inject_method(0){|start, n| start + n}
+	  smelly_old_method = array.inject(0){|start, n| start + n}
+	  expect(new_method).to eq smelly_old_method
+	end
+end
   	
 end
