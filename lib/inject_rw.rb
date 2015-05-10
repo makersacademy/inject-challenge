2442682bc18ac835f15f40a07a4c4614fc3af87c
andygout <andygout@hotmail.co.uk>
2015-05-10 16:57:34 +0100

    Working: add/multiply/starting point

--- lib/inject_rw.rb
+++ lib/inject_rw.rb
@@ -1,13 +1,25 @@
 class Array
 
-  def inject_rw(input)
-    output = 0
-    input.each do |n|
-      output += n
+  def inject_rw(accumulator = self.first, &proc)
+    self.shift if accumulator == self.first
+    self.each do |element|
+      accumulator = proc.call(accumulator, element)
     end
-    output
+    accumulator
   end
 
 end
+
+# [1,2,3].inject_rw{ |a, e| a + e } # => 6
+
+# [1,2,3].inject_rw{ |a, e| a * e } # => 6
+
+# [1,2,3].inject_rw(10){ |a, e| a + e } # => 16
+
+# [1,2,3].inject_rw(10){ |a, e| a * e } # => 60
+
+# [1,2,3].inject_rw(:+) # => 6
+
+# [1,2,3].inject_rw(10, :+) # => 60
 
 # expect([1,2,3,4].inject_rw() { |sum, n| sum + n}).to eq [1,2,3,4].inject{|sum, n| sum + n}
--- spec/inject_rw_spec.rb
+++ spec/inject_rw_spec.rb
@@ -2,8 +2,52 @@ require 'inject_rw'
 
 describe Array do
 
-  it 'sums an array of numbers' do
-  expect([1,2,3,4].inject_rw(0) { |sum, n| sum + n }).to eq [1,2,3,4].inject(0) { |sum, n| sum + n }
+  it 'Can add' do
+    expect([1,2,3].inject_rw{ |a, e| a + e }).to eq 6
+  end
+
+  it 'Can multiply' do
+    expect([1,2,3].inject_rw{ |a, e| a * e }).to eq 6
+  end
+
+  it 'Can add with a starting point' do
+    expect([1,2,3].inject_rw(10){ |a, e| a + e }).to eq 16
+  end
+
+  it 'Can multiply with a starting point' do
+    expect([1,2,3].inject_rw(10){ |a, e| a * e }).to eq 60
+  end
+
+  xit 'Can add with a symbol' do
+    expect([1,2,3].inject_rw(:+)).to eq 6
+  end
+
+  xit 'Can add with a starting point and a symbol' do
+    expect([1,2,3].inject_rw(10, :*)).to eq 60
   end
 
 end
+
+  # it 'Can add' do
+  #   expect([1,2,3].inject{ |a, e| a + e }).to eq 6
+  # end
+
+  # it 'Can multiply' do
+  #   expect([1,2,3].inject{ |a, e| a * e }).to eq 6
+  # end
+
+  # it 'Can add with a starting point' do
+  #   expect([1,2,3].inject(10){ |a, e| a + e }).to eq 16
+  # end
+
+  # it 'Can multiply with a starting point' do
+  #   expect([1,2,3].inject(10){ |a, e| a * e }).to eq 60
+  # end
+
+  # it 'Can add with a symbol' do
+  #   expect([1,2,3].inject(:+)).to eq 6
+  # end
+
+  # it 'Can add with a starting point and a symbol' do
+  #   expect([1,2,3].inject(10, :*)).to eq 60
+  # end
