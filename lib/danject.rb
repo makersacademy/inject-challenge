8b1d7de67b1965c0755c758d2144272148db8666
Daniel Johnston <macademy@danieljohnston.co.uk>
2015-05-10 02:19:52 +0100

    inject with symbols working, tests all pass

--- lib/danject.rb
+++ lib/danject.rb
@@ -1,16 +1,31 @@
 module Enumerable
   def danject(*accum)
-    if accum.empty?
-      accum = self.first
-      self.drop(1).each do |item|
-        accum = yield(accum, item)
+    if block_given? # Are we handling a symbol or block inject?
+      if accum.empty?
+        accum = self.first
+        self.drop(1).each do |item|
+          accum = yield(accum, item)
+        end
+      else
+        accum = accum[0]
+        self.each do |item|
+          accum = yield(accum, item)
+        end
       end
+      return accum
     else
-      accum = accum[0]
-      self.each do |item|
-        accum = yield(accum, item)
+      if accum.count == 1 # Symbol, no initial value
+        result = self.first
+        self.drop(1).each do |item|
+          result = result.send(accum[0], item)
+        end
+      else # Initial value followed by symbol
+        result = accum[0]
+        self.each do |item|
+          result = result.send(accum[1], item)
+        end
       end
+      return result
     end
-    accum
   end
 end
--- spec/inject_with_accum_and_code_block_spec.rb
+++ spec/inject_with_accum_and_code_block_spec.rb
@@ -1,6 +1,6 @@
 require 'danject'
 
-describe Array do
+describe Array do # This doesn't match to e.g. hashes
 
   it 'can calculate sum of an array' do
     expect((0..24).to_a.danject { |accum, item| accum + item }
@@ -23,5 +23,15 @@ describe Array do
   it 'can calculate sum of an array with an initial value passed to it' do
     expect((0..24).to_a.danject(5) { |accum, item| accum + item }
     ).to eq(305)
+  end
+
+  it 'can flatten a hash with an initial value passed to it' do
+    hash = Hash.new
+    hash["one"] = 37
+    hash["two"] = 42
+    expect(hash.danject({ "zero" => 32 }
+    ) { |accum, item| accum.to_s + item.flatten.to_s }
+    ).to eq(hash.inject({ "zero" => 32 }
+    ) { |accum, item| accum.to_s + item.flatten.to_s })
   end
 end
