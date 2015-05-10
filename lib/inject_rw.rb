039d7b13086af791d81da93c2f119e630ec50284
andygout <andygout@hotmail.co.uk>
2015-05-10 18:47:32 +0100

    Method refactored

--- lib/inject_rw.rb
+++ lib/inject_rw.rb
@@ -3,13 +3,10 @@ class Array
   def inject_rw(*args, &proc)
     accumulator = self.first
     args.each do |arg|
-      accumulator = arg if arg.is_a?(Integer)
-      proc = arg.to_proc if arg.is_a?(Symbol)
+      arg.is_a?(Integer) ? accumulator = arg : proc = arg.to_proc
     end
     self.shift if accumulator == self.first
-    self.each do |element|
-      accumulator = proc.call(accumulator, element)
-    end
+    self.each { |element| accumulator = proc.call(accumulator, element) }
     accumulator
   end
 
