ce1fc6355a35a72a3ee2e8538c0bd3c7a0bfebf1
Salman <salman.karim@hotmail.com>
2015-06-19 17:59:33 +0100

    Method supports strings and integers

---
+++ lib/inject.rb
@@ -0,0 +1,30 @@
+class Array
+
+  def new_inject_block(startvalue = "empty", &block)
+    index = 0
+    result = startvalue if startvalue.is_a?(Fixnum)
+    result = 0 if self[0].is_a?(Fixnum) && startvalue.is_a?(String)
+    result = self[0] if self[0].is_a?(String)
+    while index < self.length
+      element = self[index]
+      result = block.call(result,element)
+      index +=1
+    end
+    result
+  end
+
+  #
+  # def new_inject_yield(argument)
+  #   index = 0
+  #   result = startvalue
+  #   element = self[index]
+  #   while index <= self.length
+  #     result = yield(result,element)
+  #     index +=1
+  #   end
+  #   result
+  # end
+  #
+
+
+end
