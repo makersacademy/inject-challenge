bad4812ac72db31b320cf0ae133e820dbad16d4f
Robert Morgan <robbie.morgan@gmail.com>
2015-06-20 16:59:15 +0100

    Method working with tests

---
+++ lib/inject.rb
@@ -0,0 +1,14 @@
+class Array
+
+  def new_inject(initial_value = self.first, &block)
+    if initial_value == self.first then self.shift
+      
+    end
+    self.each do |e|
+      initial_value = block.call(initial_value, e)
+    end
+    p initial_value
+  end
+
+end
+
---
+++ spec/inject_spec.rb
@@ -0,0 +1,33 @@
+require 'inject'
+
+describe Array do
+  it 'sums numbers' do
+    expect([4,2,3,9].new_inject(0) {|res,ele| res + ele}).to eq 18
+    expect([1,2,3,4].new_inject(0) {|res,ele| res + ele}).to eq 10
+  end
+
+  it 'works without a starting parameter' do
+    expect([3,9,4].new_inject {|res,ele| res - ele}).to eq -10
+  end
+
+  it 'can take a numeric starting parameter' do
+    expect([7,3,1,6].new_inject(10) {|res,ele| res + ele}).to eq 27
+  end
+
+  it 'can multiply' do
+    expect([2,3,4].new_inject(0) {|res,ele| res * ele}).to eq 0
+  end
+
+  it 'can multiply with a numeric parameter' do
+    expect([2,3,4].new_inject(4) {|res,ele| res * ele}).to eq 96
+  end
+
+  it 'can work with strings' do
+    expect(["this","is","a","test"].new_inject {|res,ele| res + ele}).to eq "thisisatest"
+  end
+
+  it 'can work with strings and a string parameter' do
+    expect(["this","is","a","test"].new_inject("new") {|res,ele| res + ele}).to eq "newthisisatest"
+  end
+end
+
