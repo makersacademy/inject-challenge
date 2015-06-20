985edd9b0c6c36f498b90862941307baeb56054d
TheoLeanse <theoleanse@gmail.com>
2015-06-20 13:23:04 +0100

    refactors using #times

--- lib/shoot.rb
+++ lib/shoot.rb
@@ -2,7 +2,7 @@ class ModifiedArray < Array
 
   def shoot accumulator=0
     #if accumulator == :+
-    size.times { |el| accumulator = yield accumulator, self[el] } if block_given?
+    size.times { |i| accumulator = yield accumulator, self[i] } if block_given?
     #   c= 0
     #   until size == c
     #     accumulator = yield accumulator, self[c]
