8ff8aa7b0ac96b71248532cc001de550bbc59587
Michael Arnott <arnottmj@aol.com>
2015-06-21 14:26:39 +0100

    added code for when only a symbol is used (all operators)

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,22 @@
 class Array
 
   def michael_inject(argument = self[0])
+
+    if argument == :+
+      return self.michael_inject{|x,y| x + y}
+    end
+
+    if argument == :-
+      return self.michael_inject{|x,y| x - y}
+    end
+
+    if argument == :*
+      return self.michael_inject{|x,y| x * y}
+    end
+
+    if argument == :/
+      return self.michael_inject{|x,y| x / y}
+    end
     
     sliced = self.slice(1, self.length - 1)
     total = argument
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -7,6 +7,22 @@ describe Array do
      expect([1,2,3,4,5].michael_inject{|x,y| x + y}).to eq 15
    end
 
+   it 'work with only a symbol (division)' do
+    expect([10,2,2].michael_inject(:/)).to eq 2
+   end
+
+   it 'work with only a symbol (subtraction)' do
+    expect([3,4,5].michael_inject(:-)).to eq -6
+   end
+
+   it 'work with only a symbol (multiplication' do
+    expect([6,7,8].michael_inject(:*)).to eq 336
+   end
+
+   it 'work with only a symbol (addition)' do
+    expect([10,2,2].michael_inject(:+)).to eq 14
+   end
+
  end
 
 end
