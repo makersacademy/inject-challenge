959c7b3e39e29bfe42f22f20549740a3d604a40a
Michael Arnott <arnottmj@aol.com>
2015-06-21 14:53:59 +0100

    works with an initial starting value and a symbol

--- lib/array.rb
+++ lib/array.rb
@@ -1,36 +1,45 @@
 class Array
 
-  def michael_inject(argument = 'holder')
+  def michael_inject(*args)
 
-    if argument == :+
-        return self.michael_inject{|x,y| x + y}
+    array = self.to_a
+    arguments = args.to_a
+    symbols = arguments.select {|i| i.instance_of? Symbol}
+    numbers = arguments.select {|i| i.instance_of? Fixnum}
+
+    if numbers.length != 0 && symbols.length !=0
+      return array.michael_inject(numbers.first){|x,y| x + y} if symbols.first == :+
+      return array.michael_inject(numbers.first){|x,y| x - y} if symbols.first == :-
+      return array.michael_inject(numbers.first){|x,y| x * y} if symbols.first == :*
+      return array.michael_inject(numbers.first){|x,y| x / y} if symbols.first == :/
+
+
     end
 
-    if argument == :-
-        return self.michael_inject{|x,y| x - y}
-    end
-
-    if argument == :*
-      return self.michael_inject{|x,y| x * y}
-    end
-
-    if argument == :/
-      return self.michael_inject{|x,y| x / y}
-    end
-
-    if argument != 'holder'
-      total = argument
-      self.each do |i|
-        total = yield([total,i])
-      end
+    if numbers.length !=0 && symbols.length == 0
+      total = numbers.first
+      self.each {|i| total = yield([total,i])}
       return total
-    else
-      sliced = self.slice(1, self.length - 1)
+
+    end
+
+
+    if numbers.length == 0 && symbols.length != 0
+      return array.michael_inject{|x,y| x + y} if arguments.include?(:+)
+      return array.michael_inject{|x,y| x - y} if arguments.include?(:-)
+      return array.michael_inject{|x,y| x * y} if arguments.include?(:*)
+      return array.michael_inject{|x,y| x / y} if arguments.include?(:/)
+
+    end
+
+
+    if arguments.empty?
       total = self[0]
-      sliced.each do |i|
-        total = yield([total,i])
-      end
+      sliced = self.slice(1, self.length - 1)
+      sliced.each {|i| total = yield([total,i])}
       return total
     end
+
   end
+
 end
