7ca0dbbe82743e7d2470ac7c99ffc64afc7be87b
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro.local>
2015-06-21 20:10:26 +0100

    style corrections

--- lib/injection.rb
+++ lib/injection.rb
@@ -4,10 +4,10 @@ class Array
     array = self.dup
     accumulator = array.shift
     if block_given?
-      array.each {|element| accumulator = yield accumulator, element}
+      array.each { |element| accumulator = yield accumulator, element }
       accumulator
     else
-      array.each {|element| accumulator = accumulator.send args[0], element}
+      array.each { |element| accumulator = accumulator.send args[0], element }
       accumulator
     end
   end
--- spec/injection_spec.rb
+++ spec/injection_spec.rb
@@ -2,62 +2,62 @@ require 'injection'
 
 describe Array do
 
-	describe 'injection' do
+  describe 'injection' do
 
 	  it { is_expected.to respond_to :injection }
 
     context 'with a block' do
 
       it 'can add numbers together' do
-        expect([1,2,3,4].injection{|accumulator, element| accumulator + element}).to eq 10
+        expect([1, 2, 3, 4].injection { |acc, el| acc + el }).to eq 10
       end
 
       it 'can multiply numbers together' do
-        expect([1,2,3,4].injection{|accumulator, element| accumulator * element}).to eq 24
+        expect([1, 2, 3, 4].injection { |acc, el| ac * el }).to eq 24
       end
 
       it 'can minus numbers together' do
-        expect([1,2,3,4].injection{|accumulator, element| accumulator - element}).to eq -8
+        expect([1, 2, 3, 4].injection { |acc, el| acc - el }).to eq -8
       end
 
       it 'can divide numbers together' do
-        expect([1,2,3,4].injection{|accumulator, element| accumulator / element}).to eq 0
+        expect([1, 2, 3, 4].injection { |acc, el| acc / el }).to eq 0
       end
 
       it 'can divide numbers and return the remainder' do
-        expect([1,2,3,4].injection{|accumulator, element| accumulator % element}).to eq 1
+        expect([1, 2, 3, 4].injection { |acc, el| acc % el }).to eq 1
       end
 
-      it 'can raise the first element in the shifted array to the power of the other elements' do
-        expect([5,2].injection{|accumulator, element| accumulator ** element}).to eq 25
-      end
-    end
-
-    context 'when given a symbol as an argument' do
-
-      it 'will add numbers together' do
-        expect([1,2,3,4].injection(:+)).to eq 10
-      end
-
-      it 'will multiply numbers together' do
-        expect([1,2,3,4].injection(:*)).to eq 24
-      end
-
-      it 'will minus numbers together' do
-        expect([1,2,3,4].injection(:-)).to eq -8
-      end
-
-      it 'will divide numbers together' do
-        expect([1,2,3,4].injection(:/)).to eq 0
-      end
-
-      it 'will divide numbers together and return the remainder' do
-        expect([1,2,3,4].injection(:%)).to eq 1
-      end
-
-      it 'will raise the first element in the shifted array to the power of the other elements' do
-        expect([5,2].injection(:**)).to eq 25
+      it 'raises elements to the power of the other elements' do
+        expect([5, 2].injection { |acc, el| acc ** el }).to eq 25
       end
     end
+
+      context 'when given a symbol as an argument' do
+
+        it 'will add numbers together' do
+          expect([1, 2, 3, 4].injection(:+)).to eq 10
+        end
+
+        it 'will multiply numbers together' do
+          expect([1, 2, 3, 4].injection(:*)).to eq 24
+        end
+
+        it 'will minus numbers together' do
+          expect([1, 2, 3, 4].injection(:-)).to eq -8
+        end
+
+        it 'will divide numbers together' do
+          expect([1, 2, 3, 4].injection(:/)).to eq 0
+        end
+
+        it 'will divide numbers together and return the remainder' do
+          expect([1, 2, 3, 4].injection(:%)).to eq 1
+        end
+
+        it 'raises element to the power of the other elements' do
+          expect([5, 2].injection(:**)).to eq 25
+        end
+      end
   end
 end
