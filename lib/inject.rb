f478e68168aba96436b539cf664d8527e1db2285
Salman <salman.karim@hotmail.com>
2015-06-22 08:27:13 +0100

    Supports symbols, strings and integers

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,11 +2,18 @@ class Array
 
   attr_accessor :index, :result
 
-  def new_inject_sym(startvalue = 'empty', symbol)
-    # startvalue.is_a?(Fixnum) ? index = 0 : index = 1
-    # result = startvalue if startvalue.is_a?(Fixnum)
-    # result = self[0] if self[0].is_a?(Fixnum) && startvalue.is_a?(String)
-    set_result(startvalue,symbol)
+  def new_inject(startvalue = 'empty', arg2 = nil, &block)
+    if block_given?
+      self.new_inject_block(startvalue, &block)
+    elsif startvalue.is_a?(Symbol)
+      self.new_inject_sym_wo_argument(startvalue)
+    else
+      self.new_inject_sym(startvalue,arg2)
+    end
+  end
+
+  def new_inject_sym_wo_argument(symbol)
+    set_result(symbol)
     while index < self.length
       element = self[index]
       self.result = result.send symbol, element
@@ -15,32 +22,39 @@ class Array
     result
   end
 
-  def set_result(startvalue = "empty", arg2)
-    startvalue.is_a?(Fixnum) ? self.index = 0 : self.index = 1
-    self.result = startvalue if startvalue.is_a?(Fixnum)
-    self.result = self[0] if self[0].is_a?(Fixnum) && startvalue.is_a?(String)
-    self.result = self[0] if self[0].is_a?(String)
-    p result
-    p index
-  end
-
-
-
-
-
-  def new_inject_block(startvalue = "empty", &block)
-    # startvalue.is_a?(Fixnum) ? index = 0 : index = 1
-    # result = startvalue if startvalue.is_a?(Fixnum)
-    # result = self[0] if self[0].is_a?(Fixnum) && startvalue.is_a?(String)
-    # result = self[0] if self[0].is_a?(String)
-    set_result(startvalue, &block)
+  def new_inject_sym(startvalue, symbol)
+    set_result(startvalue)
     while index < self.length
       element = self[index]
-      self.result = block.call(result,element)
+      self.result = result.send symbol, element
+      self.index += 1
+    end
+    result
+  end
+
+  def new_inject_block(startvalue = 'empty', &block)
+    set_result(startvalue)
+    while index < self.length
+      element = self[index]
+      self.result = block.call(result, element)
       self.index +=1
     end
     result
   end
+
+  def set_result(startvalue)
+    startvalue.is_a?(Fixnum) ? self.index = 0 : self.index = 1
+    self.result = self[0] if startvalue.is_a?(Symbol)
+    self.result = startvalue if startvalue.is_a?(Fixnum)
+    self.result = self[0] if self[0].is_a?(Fixnum) && startvalue.is_a?(String)
+    self.result = self[0] if self[0].is_a?(String)
+  end
+
+
+
+
+
+
 
 
   #
