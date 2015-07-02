3689ac32b7ddf3773b596a37cb218bfe4d49a963
Christian Miller <cwgmiller@gmail.com>
2015-07-02 21:24:09 +0100

    Tests Outlined

---
+++ spec/new_inject_method_spec.rb
@@ -0,0 +1,30 @@
+require 'new_inject_method.rb'
+
+describe Array do
+
+  context 'addition' do
+    it 'can add numbers together' do
+    end
+
+    it 'can add strings together' do
+    end
+
+    it 'can add numbers to an input' do
+    end
+  end
+
+  context 'subtraction' do
+    it 'can subtract numbers from the input' do
+    end
+  end
+
+  context 'multiplication' do
+    it 'can multiply the input' do
+    end
+  end
+
+  context 'division' do
+    it 'can divide the input' do
+    end
+  end
+end
