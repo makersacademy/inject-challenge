5d03453f91fb275e77366ae8517f56ed71bff2e1
Christian Miller <cwgmiller@gmail.com>
2015-07-03 00:39:51 +0100

    Inject Challenge Complete

--- lib/new_inject_method.rb
+++ lib/new_inject_method.rb
@@ -1,10 +1,17 @@
 class Array
 
-  def christian_inject (input = 0, &equation)
+  def christian_inject (input = self.first, &equation)
+    
+    if input == self.first
+      self.shift
+    end
+    
     self.each do |item|
       input = equation.call(input,item)
     end
+    
       input
-  end
   
+  end
+
 end
