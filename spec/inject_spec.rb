17f26ccefe40649791b8beaa881623f291416aa1
lewis <ljones140@gmail.com>
2015-08-01 13:12:20 +0100

    passing subtraction tests

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -2,9 +2,25 @@ require 'open_array'
 
 describe Array do
 
+  test_array = [1,2,3,4,rand(50),rand(10),rand(1),12364]
+
   describe '#my_inject' do
-    it 'sums simple number array' do
+
+    it 'sums numbers' do
       expect([1,2,3].my_inject{ |sum, next_number | sum + next_number }).to eq(6)
     end
+
+    it 'sums more numbers' do
+      expect(test_array.my_inject{ |sum, next_number | sum + next_number }).to eq(test_array.inject { |sum, next_number | sum + next_number })
+    end
+
+    it 'subtracts numbers' do
+      expect([5,2].my_inject{ |sum, number| sum - number } ).to eq(3)
+    end
+
+    it 'subtracts more numbers' do
+      expect(test_array.my_inject{ |sum, number| sum - number }).to eq(test_array.inject { |sum, number| sum - number })
+    end
+
   end
 end
