677b606e52732f159fac560868167a871dcd1d1b
Haroon <haroon.hassan@gmail.com>
2015-08-01 17:19:12 +0100

    Cleaned up formating a bit, but still needs some vigorous refactoring

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,38 +1,58 @@
 class Array
 
-  def my_inject inital_val=nil, &block
-    if inital_val == nil
+  def my_inject inital_val=nil, &block # 32 . 
+   
+    # nil_initial_value.and_string_inject if (initial_val == nil && self[0].class == String)
+    # nil_initial_value.and_numbers_inject if (initial_val == nil && self[0].class != String)
+    # initial_value.and_string_inject if (initial_val != nil && self[0].class == String)
+    # initial_value.and_numbers_inject if (initial_val != nil && self[0] != String)
 
-    accumulator = self[0]
-    arry = self.drop(1)
-    if accumulator.class == String
+
+    if inital_val == nil
+      accumulator = self[0]
+      arry = self.drop(1)
+      if accumulator.class == String
         arry.each do |x|
         accumulator = block.call(accumulator, x)
         end 
-    else 
+      else 
        arry.each do |x|
        accumulator = block.call(accumulator, x).to_f
        end
-    end
-
+      end
     else
-
       accumulator = inital_val
-    if accumulator.class == String
+      if accumulator.class == String
         self.each do |x|
         accumulator = block.call(accumulator, x)
-        end 
-    else 
+        end
+      else 
        self.each do |x|
        accumulator = block.call(accumulator, x).to_f
-       end
-
+      end
     end
 
     end
     accumulator
   end
+
+  # def nil_initial_value
+  # end
+
+  # def initial_value
+  # end
+
+  # def and_string_inject
+  # end
+
+  # def and_number_inject
+  # end
+
+
 end
+
+
+
 
     # string_inject if accumulator.class == String
     # integer_inject if accumulator.class == Fixnum
