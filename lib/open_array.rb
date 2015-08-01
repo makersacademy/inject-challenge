dff3b61b264c48ef39823ae0524255bad32cfdaa
lewis <ljones140@gmail.com>
2015-08-01 13:37:06 +0100

    refactored passings subtraction tests

--- lib/open_array.rb
+++ lib/open_array.rb
@@ -2,12 +2,23 @@ class Array
   def my_inject &block
    return self[0] unless self.count > 1
    self[1] = yield(self[0], self[1])
-   self.delete(self[0])
-   self.my_inject &block
+   self[1..-1].my_inject &block
   end
 end
 
 
-#p [1,2,3].inject { |sum, next_number | sum + next_number }
-p [5,5,5].my_inject { |sum, next_number | sum + next_number }
+p [2,3,4].inject { |sum, next_number | sum + next_number }
+p [2,3,4].my_inject { |sum, next_number | sum + next_number }
 
+#test = [1,2,3,4]
+
+test_array = [2,3,4]
+test_array.inject { |sum, next_number | sum + next_number }
+
+p [2,3,4].my_inject { |sum, number| sum - number }
+
+test_array = [2,3,4]
+
+p test_array.inject { |sum, number| sum - number }
+
+#p test[1..-1]
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -2,16 +2,18 @@ require 'open_array'
 
 describe Array do
 
-  test_array = [1,2,3,4,rand(50),rand(10),rand(1),12364]
+  test_array = [2,3,4]
+  add_result = test_array.inject { |sum, next_number | sum + next_number }
+  subtract_result = test_array.inject { |sum, number| sum - number }
 
   describe '#my_inject' do
 
     it 'sums numbers' do
-      expect([1,2,3].my_inject{ |sum, next_number | sum + next_number }).to eq(6)
+      expect([5,5,5].my_inject{ |sum, next_number | sum + next_number }).to eq(15)
     end
 
     it 'sums more numbers' do
-      expect(test_array.my_inject{ |sum, next_number | sum + next_number }).to eq(test_array.inject { |sum, next_number | sum + next_number })
+      expect([2,3,4].my_inject{ |sum, next_number | sum + next_number }).to eq(add_result)
     end
 
     it 'subtracts numbers' do
@@ -19,7 +21,7 @@ describe Array do
     end
 
     it 'subtracts more numbers' do
-      expect(test_array.my_inject{ |sum, number| sum - number }).to eq(test_array.inject { |sum, number| sum - number })
+      expect([2,3,4].my_inject{ |sum, number| sum - number }).to eq(subtract_result)
     end
 
   end
