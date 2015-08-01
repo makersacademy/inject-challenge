748c8763ccd9138ab764dc166966ac5403a9c725
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-01 21:02:53 +0100

    Inject now seems to behave as expected. Still does not work without arguments

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,19 +1,11 @@
 
 class Array
   def injjject(argument = nil)
-    if argument != nil
-      result = yield(argument, self[0])
-      self[1..-1].each do |x|
-        result += yield(argument, x)
-      end
-    else
-      result = yield(self[0], self[1])
-      self[1..-2].each do |x|
-        result += yield(self[0], x)
-      end
-    end
+      result = argument
+      result = yield(result, self[0])
+      self[1..-1].each { |element| result = yield(result, element) }
     result
   end
 end
 
- p [1,2,3,4,5,6].injjject{|result, element| result + element}
+ p [1,2,3,4].injjject(0){|result, element| result + element}
