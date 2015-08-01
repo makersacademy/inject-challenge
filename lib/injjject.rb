66214ee129499f19fbb6fc2d199545c37abb1aaf
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-02 00:18:51 +0100

    refactoring

--- lib/injjject.rb
+++ lib/injjject.rb
@@ -2,12 +2,10 @@
 class Array
   def injjject(argument = nil)
     if argument
-      result = argument
-      result = yield(result, self[0])
+      result = yield(argument, self[0])
       self[1..-1].each { |element| result = yield(result, element) }
     else
-      result = self[0]
-      result = yield(result, self[1])
+      result = yield(self[0], self[1])
       self[2..-1].each { |element| result = yield(result, element) }
     end
     result
