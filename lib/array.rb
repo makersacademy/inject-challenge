f3ff7689e7f130266a4868b2c8a2f678f26cd012
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-02 12:30:38 +0100

    Added some more tests

--- lib/array.rb
+++ lib/array.rb
@@ -1,5 +1,6 @@
 class Array
-	def new_inject(arg, &block)
+	def new_inject(arg=self[0], &block)
+		self.shift if arg == self[0]
 		self.each do |element|
 			arg = yield(arg, element)
 		end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,7 +5,11 @@ describe Array do
 	it { is_expected.to respond_to(:new_inject) }
 
 	it 'expect new_inject to add numbers in array' do
-		expect([1,2,3,4].new_inject(0) { |result, element| result + element }).to eq(10)
+		expect([1,2,3,4].new_inject { |result, element| result + element }).to eq(10)
+	end
+
+	it 'expect new_inject to add strings in an array together' do
+		expect(['I', 'hope', 'this', 'works'].new_inject { |result, element| result + ' ' + element}).to eq('I hope this works')
 	end
 
 end
