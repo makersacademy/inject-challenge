904e3b63b262f4bf99917a3d76c784bbb34b1044
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-08-02 17:48:48 +0100

    styling fixes

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,7 +1,7 @@
 require 'array'
 
 describe Array do
-  subject { [5,6,7,8,9,10] }
+  subject { [5, 6, 7, 8, 9, 10] }
 
   it "responds to crush_down" do
     expect(subject).to respond_to(:crush_down)
@@ -20,12 +20,12 @@ describe Array do
       end
 
       it "injects divide into array and returns value" do
-        array = [10,5,1]
+        array = [10, 5, 1]
         expect(array.crush_down { |sum, n| sum / n }).to eq 2
       end
 
       it "injects modulo into array and returns value" do
-        array = [10,7,2]
+        array = [10, 7, 2]
         expect(array.crush_down { |sum, n| sum % n }).to eq 1
       end
 
@@ -46,17 +46,17 @@ describe Array do
       end
 
       it "injects subtract into array and returns value" do
-        arr = [5,2]
+        arr = [5, 2]
         expect(arr.crush_down(10) { |sum, n| sum - n }).to eq 3
       end
 
       it "injects divide into array and returns value" do
-        arr = [5,2]
+        arr = [5, 2]
         expect(arr.crush_down(10) { |sum, n| sum / n }).to eq 1
       end
 
       it "injects modulo into array and returns value" do
-        arr = [4,2]
+        arr = [4, 2]
         expect(arr.crush_down(11) { |sum, n| sum % n }).to eq 1
       end
 
@@ -73,17 +73,17 @@ describe Array do
       end
 
       it "injects divide into array and returns value" do
-        array = [10,5,1]
+        array = [10, 5, 1]
         expect(array.crush_down(:/)).to eq 2
       end
 
       it "injects modulo into array and returns value" do
-        array = [10,7,2]
+        array = [10, 7, 2]
         expect(array.crush_down(:%)).to eq 1
       end
 
       it "injects subtract into array and returns value" do
-        array = [10,7,2]
+        array = [10, 7, 2]
         expect(array.crush_down(:-)).to eq 1
       end
 
@@ -92,27 +92,27 @@ describe Array do
     context "with a symbol and initial value" do
 
       it "injects multiply into array and returns value" do
-        expect(subject.crush_down(1,:*)).to eq 151200
+        expect(subject.crush_down(1, :*)).to eq 151200
       end
 
       it "injects plus into array and returns value" do
-        expect(subject.crush_down(1,:+)).to eq 46
+        expect(subject.crush_down(1, :+)).to eq 46
       end
 
 
       it "injects subtract into array and returns value" do
-        arr = [5,2]
-        expect(arr.crush_down(10,:-)).to eq 3
+        arr = [5, 2]
+        expect(arr.crush_down(10, :-)).to eq 3
       end
 
       it "injects divide into array and returns value" do
-        arr = [5,2]
-        expect(arr.crush_down(10,:/)).to eq 1
+        arr = [5, 2]
+        expect(arr.crush_down(10, :/)).to eq 1
       end
 
       it "injects modulo into array and returns value" do
-        arr = [4,2]
-        expect(arr.crush_down(11,:%)).to eq 1
+        arr = [4, 2]
+        expect(arr.crush_down(11, :%)).to eq 1
       end
 
     end
@@ -130,12 +130,12 @@ describe Array do
         end
 
         it "injects divide into array and returns value" do
-          array = [10,5,1]
+          array = [10, 5, 1]
           expect(array.recursive_crush_down { |sum, n| sum / n }).to eq 2
         end
 
         it "injects modulo into array and returns value" do
-          array = [10,7,2]
+          array = [10, 7, 2]
           expect(array.recursive_crush_down { |sum, n| sum % n }).to eq 1
         end
 
@@ -156,17 +156,17 @@ describe Array do
         end
 
         it "injects subtract into array and returns value" do
-          arr = [5,2]
+          arr = [5, 2]
           expect(arr.recursive_crush_down(10) { |sum, n| sum - n }).to eq 3
         end
 
         it "injects divide into array and returns value" do
-          arr = [5,2]
+          arr = [5, 2]
           expect(arr.recursive_crush_down(10) { |sum, n| sum / n }).to eq 1
         end
 
         it "injects modulo into array and returns value" do
-          arr = [4,2]
+          arr = [4, 2]
           expect(arr.recursive_crush_down(11) { |sum, n| sum % n }).to eq 1
         end
 
