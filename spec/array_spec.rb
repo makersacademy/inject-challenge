ea618893c1bbb41a6c85f7bbe89a35845ba99a37
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-02 12:58:11 +0100

    Refactored

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -12,15 +12,15 @@ describe Array do
 		expect(['I', 'hope', 'this', 'works'].new_inject { |result, element| result + ' ' + element}).to eq('I hope this works')
 	end
 
-	it 'expect new_inject to return the product of numbers with argument' do
-		expect([4,5,6].new_inject(10) { |result, element| result * element }).to eq(1200)
-	end
-
 	it 'expect new_inject to create a new array' do
 		expect([1,2,3,4,5,6,7,8,9,10].new_inject([]) do |result, element|
 			result << element if element % 2 == 0
 			result
 		end).to eq([2,4,6,8,10])
+	end
+
+	it 'expect new_inject to return the product of numbers with argument' do
+		expect([4,5,6].new_inject(10) { |result, element| result * element }).to eq(1200)
 	end
 
 end
