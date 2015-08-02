f9fce7563bc5830aaa5e57a010fc558422e8f5b6
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-02 14:44:30 +0100

    Further reduced line length - hound violation

--- spec/new_inject_spec.rb
+++ spec/new_inject_spec.rb
@@ -1,8 +1,8 @@
 require 'new_inject'
 
 describe Array do
-  array_numbers = [1, 2, 3, 4, 5]
-  array_words = ["cat", "sheep", "bear"]
+  arr1 = [1, 2, 3, 4, 5]
+  arr2 = ["cat", "sheep", "bear"]
 
   it "can respond to new_inject" do
     expect(subject).to respond_to(:new_inject).with(1).argument
@@ -10,27 +10,27 @@ describe Array do
 
   describe "#new_inject" do
     it "accepts an addition block" do
-      expect(array_numbers.new_inject { |a, b| a + b }).to eq(15)
+      expect(arr1.new_inject { |a, b| a + b }).to eq(15)
     end
 
     it "accepts a multiplication block" do
-      expect(array_numbers.new_inject { |a, b| a * b }).to eq(120)
+      expect(arr1.new_inject { |a, b| a * b }).to eq(120)
     end
 
     it "can accept additional arguments" do
-      expect(array_numbers.new_inject(6) { |a, b| a + b }).to eq(21)
+      expect(arr1.new_inject(6) { |a, b| a + b }).to eq(21)
     end
 
     it "can find the shortest word" do
-      expect(array_words.new_inject { |m, w| m.length < w.length ? m : w }).to eq("cat")
+      expect(arr2.new_inject { |m, w| m.length < w.length ? m : w }).to eq("cat")
     end
 
     it "can accept symbols" do
-      expect(array_numbers.new_inject(:+)).to eq(15)
+      expect(arr1.new_inject(:+)).to eq(15)
     end
 
     it "can accept symbols with other arguments" do
-      expect(array_numbers.new_inject(6, :+)).to eq(21)
+      expect(arr1.new_inject(6, :+)).to eq(21)
     end
   end
 end
