3ca0cab883201a4a7c3b0e68b984ada44dd7ab0c
UpstatePedro <piers.m.holden@gmail.com>
2015-08-03 08:53:34 +0100

    Both iterative & recursive inject methods complete

--- lib/inject-challenge.rb
+++ lib/inject-challenge.rb
@@ -27,4 +27,19 @@ class MyArray < Array
     end
   end
 
+  def attempt_at_recursive_inject(i=0, result=nil, &function)
+    result ||= self[0] if result == nil
+    if i+1 == (self.length)
+      return result
+    else
+      element = self[i+1]
+      # puts "i is #{i}"
+      # puts "result is #{result}"
+      # puts "element is #{element}"
+      result = function.call(result, element)
+      i+=1
+      self.attempt_at_recursive_inject(i, result, &function)
+    end
+  end
+
 end
--- spec/inject-challenge_spec.rb
+++ spec/inject-challenge_spec.rb
@@ -36,4 +36,40 @@ describe MyArray do
       expect(subject.attempt_at_inject(5) { |r, e| r>e ? r+e : r-e }).to be( subject.inject(5) { |r, e| r>e ? r+e : r-e })
     end
   end
+
+  describe '#attempt_at_recursive_inject - without initial value argument' do
+    before do
+      subject.concat([1,2,3,4,5])
+    end
+
+    it 'behaves like inject { |r,e| r*e }' do
+      expect(subject.attempt_at_recursive_inject { |r, e| r*e }).to be( subject.inject { |r, e| r*e })
+    end
+
+    it 'behaves like inject { |r,e| r+e }' do
+      expect(subject.attempt_at_recursive_inject { |r, e| r+e }).to be( subject.inject { |r, e| r+e })
+    end
+
+    it 'behaves like inject { |r,e| r>e ? r+e : r-e }' do
+      expect(subject.attempt_at_recursive_inject { |r, e| r>e ? r+e : r-e }).to be( subject.inject { |r, e| r>e ? r+e : r-e })
+    end
+  end
+
+  # describe '#attempt_at_recursive_inject - with initial value argument' do
+  #   before do
+  #     subject.concat([1,2,3,4,5])
+  #   end
+  #
+  #   it 'behaves like inject { |r,e| r*e }' do
+  #     expect(subject.attempt_at_recursive_inject(5) { |r, e| r*e }).to be( subject.inject(5) { |r, e| r*e })
+  #   end
+  #
+  #   it 'behaves like inject { |r,e| r+e }' do
+  #     expect(subject.attempt_at_recursive_inject(5) { |r, e| r+e }).to be( subject.inject(5) { |r, e| r+e })
+  #   end
+  #
+  #   it 'behaves like inject { |r,e| r>e ? r+e : r-e }' do
+  #     expect(subject.attempt_at_recursive_inject(5) { |r, e| r>e ? r+e : r-e }).to be( subject.inject(5) { |r, e| r>e ? r+e : r-e })
+  #   end
+  # end
 end
