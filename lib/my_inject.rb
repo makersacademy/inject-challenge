21523b4d60bd4069c1decb5937385b6e0ef34410
Haroon <haroon.hassan@gmail.com>
2015-08-03 08:11:10 +0100

    Update my_inject.rb

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,13 +1,6 @@
 class Array
 
-  def my_inject inital_val=nil, &block # 32 . 
-   
-    # nil_initial_value.and_string_inject if (initial_val == nil && self[0].class == String)
-    # nil_initial_value.and_numbers_inject if (initial_val == nil && self[0].class != String)
-    # initial_value.and_string_inject if (initial_val != nil && self[0].class == String)
-    # initial_value.and_numbers_inject if (initial_val != nil && self[0] != String)
-
-
+  def my_inject inital_val=nil, &block
     if inital_val == nil
       accumulator = self[0]
       arry = self.drop(1)
@@ -31,30 +24,10 @@ class Array
        accumulator = block.call(accumulator, x).to_f
       end
     end
-
     end
     accumulator
   end
-
-  # def nil_initial_value
-  # end
-
-  # def initial_value
-  # end
-
-  # def and_string_inject
-  # end
-
-  # def and_number_inject
-  # end
-
-
 end
 
 
-
-
-    # string_inject if accumulator.class == String
-    # integer_inject if accumulator.class == Fixnum
-    # float_inject if accumulator.class == Float
 
