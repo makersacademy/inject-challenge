34b49c45a4ef2b8286ef2c5171e9ab63a1f75572
Makers Laptop 22 <makerslaptop22@makerss-mbp-6.home>
2015-08-03 08:32:34 +0100

    Edited test files

--- spec/inject_method_spec.rb
+++ spec/inject_method_spec.rb
@@ -3,10 +3,10 @@ require 'inject_method'
 describe Array do
   let(:array) { [1,2,3] }
 
-  add = Proc.new { |plus, i|plus + i }
-  subtract = Proc.new { |plus, i|plus - i }
-  multiply = Proc.new { |plus, i|plus * i }
-  divide = Proc.new { |plus, i|plus / i }
+  let (:add) { Proc.new { |plus, i|plus + i } }
+  let (:subtract) { Proc.new { |plus, i|plus - i } }
+  let (:multiply) { Proc.new { |plus, i|plus * i } }
+  let (:divide) { Proc.new { |plus, i|plus / i } }
 
   it"Has a method owen_inject" do
     expect(subject).to respond_to :owen_inject
