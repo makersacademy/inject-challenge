71fb1f0d692bac730025387714ad732f8f9e869f
UpstatePedro <piers.m.holden@gmail.com>
2015-08-04 16:18:05 +0100

    Post-feedback amendments

--- lib/inject-challenge.rb
+++ lib/inject-challenge.rb
@@ -2,29 +2,10 @@
 class MyArray < Array
 
   def attempt_at_inject(*arg)
-    raise "#attempt_at_inject called without block" if not block_given?
-    if arg.empty?
-      i = 0
-      result = self[i]
-      (self.length-1).times do
-        element = self[i+1]
-        result = yield(result, element)
-        #puts "#{i}th result is equal to: #{result}"
-        i+=1
-      end
-      return result
-    else
-      raise "#attempt_at_inject given too many arguments for initial value" if arg.length >1
-      i = 0
-      result = arg[0]
-      (self.length).times do
-        element = self[i]
-        result = yield(result, element)
-        #puts "#{i}th result is equal to: #{result}"
-        i+=1
-      end
-      return result
-    end
+    copy = self.dup
+    result = (arg.empty? ? copy.shift : arg[0])
+    copy.each{ |item|result = yield(result, item)}
+    return result
   end
 
   def attempt_at_recursive_inject(i=0, result=nil, &function)
@@ -33,9 +14,6 @@ class MyArray < Array
       return result
     else
       element = self[i+1]
-      # puts "i is #{i}"
-      # puts "result is #{result}"
-      # puts "element is #{element}"
       result = function.call(result, element)
       i+=1
       self.attempt_at_recursive_inject(i, result, &function)
--- spec/inject-challenge_spec.rb
+++ spec/inject-challenge_spec.rb
@@ -3,7 +3,7 @@ require 'inject-challenge'
 describe MyArray do
   describe '#attempt_at_inject - without initial value argument' do
     before do
-      subject.concat([1,2,3,4,5])
+      described_class.new([1,2,3,4,5])
     end
 
     it 'behaves like inject { |r,e| r*e }' do
@@ -24,15 +24,15 @@ describe MyArray do
       subject.concat([1,2,3,4,5])
     end
 
-    it 'behaves like inject { |r,e| r*e }' do
+    it 'behaves like inject(1) { |r,e| r*e }' do
       expect(subject.attempt_at_inject(5) { |r, e| r*e }).to be( subject.inject(5) { |r, e| r*e })
     end
 
-    it 'behaves like inject { |r,e| r+e }' do
+    it 'behaves like inject(1) { |r,e| r+e }' do
       expect(subject.attempt_at_inject(5) { |r, e| r+e }).to be( subject.inject(5) { |r, e| r+e })
     end
 
-    it 'behaves like inject { |r,e| r>e ? r+e : r-e }' do
+    it 'behaves like inject(1) { |r,e| r>e ? r+e : r-e }' do
       expect(subject.attempt_at_inject(5) { |r, e| r>e ? r+e : r-e }).to be( subject.inject(5) { |r, e| r>e ? r+e : r-e })
     end
   end
