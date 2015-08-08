b5e835d4945a5c6974e033469fb23a41dad83ca6
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-08 11:52:45 +0100

    Refactored with the help of my coach

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -1,31 +1,10 @@
 class Array
   def new_inject(*args)
-    n = 0
-    if args.size == 1 && !block_given?
-      start = self[0]
-      start = yield_with_symbol(self, start, args[0])
-    elsif args.size == 2 && !block_given?
-      combined = self << args[0]
-      start = combined[0]
-      start = yield_with_symbol(combined, start, args[1])
-    else
-      combined = self + args
-      start = combined[0]
-      return combined if combined.length == 1
-      while n < combined.length - 1
-        start = yield(start, combined[n + 1])
-        n += 1
-      end
-    end
+    copy = self.dup
+    symbol = args.delete(args.select{|a|a.is_a?(Symbol)}.first)
+    start = args.first || copy.shift
+    copy.each {|element| start = yield(start, element)} if block_given?
+    copy.each {|element| start = start.send(symbol, element)} if symbol
     start
-  end
-
-  def yield_with_symbol(arr, initial, symbol)
-    n = 0
-    while n < arr.length - 1
-      initial = initial.send(symbol, arr[n + 1])
-      n += 1
-    end
-    initial
   end
 end
