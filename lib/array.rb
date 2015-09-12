66de565a42c1cc7e4a27fad33a07854bbd6ee6f6
Michael Roger <bodgeuk12@gmail.com>
2015-09-12 11:32:52 +0100

    Test passed for arrays of nil values

--- lib/array.rb
+++ lib/array.rb
@@ -2,6 +2,14 @@ class Array
 
   def mick_inject(initial = nil, &block)
     return nil if count == 0
+    if all?{ |a| !a }
+      accumulator = initial
+      each do |a|
+        accumulator = block.call(accumulator, a)
+      end
+      return accumulator
+    end
+
     if !initial
       initial = self[0]
       shift
@@ -17,4 +25,8 @@ class Array
 
 end
 
+array = [nil,nil,nil,nil]
 
+p array.count
+p array.inject{|acc, x| acc = !x}
+p array.mick_inject{|acc, x| acc = !x}
