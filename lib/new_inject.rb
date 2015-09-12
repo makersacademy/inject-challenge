69f4a53d3c6dbb6d8cec1ee8e6938bab2b62b8a8
davidbebb <davidbebb@gmail.com>
2015-09-12 20:44:51 +0100

    refactord iterative inject and added recursive inject

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -1,11 +1,21 @@
 
 class Array
-  def inject! *number
+  def iterative_inject! *number
     number = [self.shift] if number == []
     self.each_with_index do |element, i|
       i == 0 ? args = [number[0], element] : args = [self[i-1], element]
-      self[i]=yield args
+      self[i] = yield args
     end
     self.last
+  end
+
+  def recursive_inject! *number, &block
+    self.unshift number[0] unless number == []
+    while self.length>1 do
+      self[1] =  block.call [self[0], self[1]]
+      self.shift
+      self.recursive_inject!(&block)
+    end
+    self[0]
   end
 end
--- spec/new_inject_spec.rb
+++ spec/new_inject_spec.rb
@@ -1,21 +1,36 @@
 require 'new_inject'
 
 describe Array do
-  it 'reponds to the inject! method' do
-    expect([]).to respond_to(:inject!)
-  end
+  context 'iterative_inject' do
+    it 'reponds to the iterative_inject! method' do
+      expect([]).to respond_to(:iterative_inject!)
+    end
 
-  it 'sums an array of integers 'do
-    array=[1,2,3,4]
-    expect(array.inject!(0) {|result, element| result + element}).to eq(10)
-  end
+    it 'sums an array of integers 'do
+      array=[1,2,3,4]
+      expect(array.iterative_inject!(0) {|result, element| result + element}).to eq(10)
+    end
 
-  it 'matches the interal inject method for this block' do
-    array=[9,2,5]
-    comparison=array.inject{|result, element| element*result}
-    expect(array.inject!{|result, element| element*result}). to eq(comparison)
+    it 'matches the interal inject method for this block' do
+      array=[9,2,5]
+      comparison=array.inject{|result, element| element*result}
+      expect(array.iterative_inject!{|result, element| element*result}). to eq(comparison)
+    end
   end
-
+  context 'recursive_inject!' do
+    it 'reponds to the recursive_inject! method' do
+      expect([]).to respond_to(:recursive_inject!)
+    end
+    it 'sums an array of integers 'do
+      array=[1,2,3,4]
+      expect(array.recursive_inject!(0) {|result, element| result + element}).to eq(10)
+    end
+    it 'matches the interal inject method for this block' do
+      array=[9,2,5]
+      comparison=array.inject{|result, element| element*result}
+      expect(array.recursive_inject!{|result, element| element*result}). to eq(comparison)
+    end
+  end
 
 
 end
