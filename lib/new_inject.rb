bfd7c6e12e395f02f38d29d77a03a94cfc8f0ccb
chweeks <chrisharryweeks@gmail.com>
2015-09-12 23:16:10 +0100

    added self asssignment shorthand

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -1,7 +1,7 @@
 class Array
   def new_inject(result = nil)
     copy = self.dup
-    result = result || copy[0]
+    result ||= copy[0]
     copy.each do |element|
       result =  yield(result, element)
     end
