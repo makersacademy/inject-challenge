9f11ab714ab305952e177251bfbf776ea3a838bb
chweeks <chrisharryweeks@gmail.com>
2015-09-12 21:34:04 +0100

    added new_inject method

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -1,4 +1,10 @@
 class Array
-  def new_inject(*args, &block)
+  def new_inject(result = nil)
+    copy = self.dup
+    result = result || copy[0]
+    copy.each do |element|
+      result =  yield(result, element)
+    end
+    result
   end
 end
--- spec/new_inject_spec.rb
+++ spec/new_inject_spec.rb
@@ -2,15 +2,15 @@ require 'new_inject'
 
 describe Array do
 
-  array = [1,2,3,4,5]
-
   describe '#new_inject' do
+
+    array = [1,2,3,4,5]
+
     it { is_expected.to respond_to :new_inject }
 
     it 'returns the sum of the array' do
       result = array.new_inject(0){|sum, x| sum += x}
-      expect(result).to be 15 
+      expect(result).to be 15
     end
   end
-
 end
