deb8a2383ab18eb758cf3d34663a59e542feba66
chn.challenger <chn.challenger@gmail.com>
2015-09-12 11:18:30 +0100

    Experimenting with passing a block to the array

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -1,7 +1,15 @@
 class Array
 
   def new_inject(&block)
-
+    result = []
+    self.each do |element|
+      result << block.call(element)
+    end
+    result
   end
 
 end
+
+ary1 = [1,2,3,4]
+array2 = ary1.new_inject{|x| x*x}
+p array2
