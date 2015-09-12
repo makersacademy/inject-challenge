319f47047a9f5a00df4ec344790fc300798b1820
Jongmin Kim <kjm141215@gmail.com>
2015-09-12 22:48:36 +0100

    Added conditional so that double_inject works without an initial value

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,15 +1,23 @@
 class Array
 
-  def double_inject(initial, &block)
+  def double_inject(initial=nil, &block)
     array = []
-    each do |el|
-      array << el
-    end
-    i = 1
-    new = block.call(initial, array[0])
-    while i < array.length
-      new = block.call(new, array[i])
-      i += 1
+    each { |el| array << el }
+    if initial
+      i = 1
+      new = block.call(initial, array[0])
+      while i < array.length
+        new = block.call(new, array[i])
+        i += 1
+      end
+    else
+      initial = array[0]
+      i = 1
+      new = block.call(initial, array[i])
+      while i + 1 < array.length
+        new = block.call(new, array[i + 1])
+        i += 1
+      end
     end
     new
   end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -2,23 +2,32 @@ require 'inject'
 
 describe Array do
   describe "#double_inject" do
-    context "With a parameter" do
+    context "With an initial value" do
       it "adds up all the numbers" do
-        expect([5,6,7,8,9,10].double_inject(0) { |memo, n| memo + n } ).to eq(45)
+        expect([5,6,7,8,9,10].double_inject(0) { |memo, n| memo + n } ).
+          to eq(45)
       end
 
       it "multiplies all the numbers" do
-        expect([1,2,3,4,5].double_inject(1) { |memo, n| memo * n }).to eq(120)
+        expect([1,2,3,4,5].double_inject(1) { |memo, n| memo * n }).
+          to eq(120)
       end
 
       it "combines strings" do
-        expect(('B'..'G').to_a.double_inject('A') { |sum, n| sum + n }).to eq('ABCDEFG')
+        expect(('B'..'G').to_a.double_inject('A') { |memo, n| memo + n }).
+          to eq('ABCDEFG')
       end
     end
 
-    context "Without a parameter" do
-      xit "adds up all the numbers" do
-        expect([5,6,7,8,9,10].double_inject { |sum, n| sum + n } ).to eq(45)
+    context "Without an initial value" do
+      it "adds up all the numbers" do
+        expect([5,6,7,8,9,10].double_inject { |memo, n| memo + n } ).
+          to eq(45)
+      end
+
+      it "combines strings" do
+        expect(('A'..'G').to_a.double_inject { |memo, n| memo + n }).
+          to eq('ABCDEFG')
       end
     end
   end
