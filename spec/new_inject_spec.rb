03c0089c8fb958086d2873f320efbc4e27473208
chn.challenger <chn.challenger@gmail.com>
2015-09-12 13:12:25 +0100

    Changed 1st test to use ruby inject for comparison

--- spec/new_inject_spec.rb
+++ spec/new_inject_spec.rb
@@ -5,10 +5,14 @@ describe Array do
     it {expect(subject).to respond_to(:new_inject).with(1).argument}
 
     it 'produce an array of squares of an array of numbers' do
-      result = [1,2,3,5].new_inject([]) do |obj, ele|
+      array = [1,2,3,5]
+      result = array.new_inject([]) do |obj, ele|
         obj << ele * ele
       end
-      expect(result).to eq([1,4,9,25])
+      norm_inj_res = array.inject([]) do |obj, ele|
+        obj << ele * ele
+      end
+      expect(result).to eq(norm_inj_res)
     end
   end
 end
