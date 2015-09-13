c4fc292fb98ed98cb272d93533b95dce08dfa18f
ana <adscarlos@gmail.com>
2015-09-13 19:53:29 +0100

    changes ternary to use  predicate

--- lib/sandwich_in.rb
+++ lib/sandwich_in.rb
@@ -1,7 +1,7 @@
 class Array
   def sandwich_in(n=nil)
     array_copy = self.dup
-    n == nil ? acc = array_copy.shift : acc = n
+    n.nil? ? acc = array_copy.shift : acc = n
     array_copy.each { |el| acc = yield(acc, el) }
     acc
   end
