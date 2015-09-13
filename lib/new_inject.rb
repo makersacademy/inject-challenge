78ea1acbaa241f6369184da408dd6326bc9ea1f4
chn.challenger <chn.challenger@gmail.com>
2015-09-13 09:26:31 +0100

    fixing

--- lib/new_inject.rb
+++ lib/new_inject.rb
@@ -68,32 +68,41 @@ class Array
     if arg == []
       return self.new_inject_without_arg &block
     else
-      arg = arg[0]
-      return self.new_inject_with_arg arg, &block
+      return self.new_inject_with_arg arg[0], &block
+    end
+  end
+
+  def recursion_inject_with_arg arg, &block
+    self_copy = self.dup
+    result = arg
+    element = self_copy.delete_at(0)
+    result = block.call(result, element)
+    if self_copy == []
+      return result
+    else
+      return self_copy.recursion_inject_with_arg result, &block
+    end
+  end
+
+  def recursion_inject_without_arg &block
+    self_copy = self.dup
+    result = self_copy.delete_at(0)
+    element = self_copy.delete_at(0)
+    puts "result - #{result} element - #{element}"
+    result = block.call(result, element)
+    if self_copy == []
+      puts "result - #{result}"
+      return result
+    else
+      return self_copy.recursion_inject_without_arg &block
     end
   end
 
   def recursion_inject *arg, &block
     if arg == []
-      self_copy = self.dup
-      result = self_copy.delete_at(0)
-      element = self_copy.delete_at(0)
-      result = block.call(result, element)
-      if self_copy == []
-        return result
-      else
-        return self_copy.recursion_inject result, &block
-      end
+      return self.recursion_inject_without_arg &block
     else
-      self_copy = self.dup
-      result = arg[0]
-      element = self_copy.delete_at(0)
-      result = block.call(result, element)
-      if self_copy == []
-        return result
-      else
-        return self_copy.recursion_inject result, &block
-      end
+      return self.recursion_inject_with_arg arg[0], &block
     end
   end
 
