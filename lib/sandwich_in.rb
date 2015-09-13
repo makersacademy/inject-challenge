a67134f2f23f8bcbd92139c15eb8e2860e9a8555
ana <adscarlos@gmail.com>
2015-09-13 19:14:39 +0100

    adds sandwich_in method and tests

--- lib/sandwich_in.rb
+++ lib/sandwich_in.rb
@@ -1,7 +1,12 @@
 class Array
-  def sandwich_in(n)
-  acc = n
-  self.each { |el| acc += el }
-  acc
+  def sandwich_in(n=nil)
+    dup_array = self.dup
+    if n == nil
+      acc = dup_array.shift
+    else
+      acc = n
+    end
+    dup_array.each { |el| acc = yield(acc, el) }
+    acc
   end
 end
--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,6 +1,8 @@
 require 'sandwich_in'
 
 describe Array do
+  let(:array) { [1,2,3,4] }
+
   context 'the sandwich_in method'
 
   it 'returns the argument given an empty array' do
@@ -8,9 +10,29 @@ describe Array do
     expect(array.sandwich_in 5).to eq(5)
   end
 
-  it 'sums all numbers in an array' do
-    array = [1,2,3,4]
-    expect(array.sandwich_in 0).to eq 10
+  it 'given 0 as an argument,sums all numbers in a array and returns 10' do
+    expect(array.sandwich_in(0) { |acc, n| acc + n}).to eq 10
+  end
+
+  it 'sums all numbers in an array without passing an argument' do
+    expect(array.sandwich_in { |acc, n| acc + n}).to eq 10
+  end
+
+  it 'multiplies all numbers in an array and returns 24' do
+    expect(array.sandwich_in { |acc, n| acc * n}).to eq 24
+  end
+
+  it 'divides all numbers in an array and returns 0' do
+    expect(array.sandwich_in { |acc, n| acc / n}).to eq 0
+  end
+
+  it 'subtracts all numbers in an array and returns -8' do
+    expect(array.sandwich_in { |acc, n| acc - n}).to eq(-8)
+  end
+
+  it 'finds the longest word in an array' do
+    words = %w(crocodile bear giraffe lion )
+    expect(words.sandwich_in { |acc, word| acc.length > word.length ? acc : word}).to eq('crocodile')
   end
 
 end
