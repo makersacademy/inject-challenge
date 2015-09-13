b8124089d29e2d8c5fba0e5da8403f0ceb0712c1
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-13 20:07:16 +0100

    Added symbol testing

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -4,15 +4,18 @@ describe Array do
 
   let(:array) {[1,2,3,4]}
 
-  add = Proc.new { |acc, elem| acc + elem }
-  sub = Proc.new { |acc, elem| acc - elem }
-  mul = Proc.new { |acc, elem| acc * elem }
-  div = Proc.new { |acc, elem| acc / elem }
-  doub = Proc.new { |acc, elem| acc << 2*elem }
-  squ = Proc.new { |acc, elem| acc << elem * elem}
 
-  describe "#my_inject using procs for testing integers" do
+  describe "#my_inject" do
 
+    context "using procs for testing integers" do
+
+    add = Proc.new { |acc, elem| acc + elem }
+    sub = Proc.new { |acc, elem| acc - elem }
+    mul = Proc.new { |acc, elem| acc * elem }
+    div = Proc.new { |acc, elem| acc / elem }
+    doub = Proc.new { |acc, elem| acc << 2*elem }
+    squ = Proc.new { |acc, elem| acc << elem * elem}
+    
       it { is_expected.to respond_to(:my_inject).with(1).argument}
 
       it "will act like inject with addition" do
@@ -22,7 +25,7 @@ describe Array do
 
       it "will act like inject with subraction" do
         result = array.my_inject { |acc, elem| acc - elem }
-        expect(array.my_inject &sub).to eq(-8)
+        expect(array.my_inject &sub).to eq(result)
       end
 
       it "will act like inject with multiplication" do
@@ -46,5 +49,27 @@ describe Array do
       end
 
     end
+
+    context 'using symbols for tesing integers' do
+
+      it 'will act like inject with addition' do
+       expect(array.my_inject(:+)).to eq(10)
+      end
+
+      it 'will act like inject with subtraction' do
+       expect(array.my_inject(:-)).to eq(-8)
+      end
+
+      it 'will act like inject with multiplication' do
+       expect(array.my_inject(:*)).to eq(24)
+      end
+
+      it 'will act like inject with division' do
+       expect(array.my_inject(:/)).to eq(0)
+      end
+
+    end
+
+  end
 
 end
