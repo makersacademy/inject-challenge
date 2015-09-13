9a0a77c7d83749ba60356348af0a4c921b5c8fec
ana <adscarlos@gmail.com>
2015-09-13 19:31:42 +0100

    refactors code

--- lib/sandwich_in.rb
+++ lib/sandwich_in.rb
@@ -1,8 +1,8 @@
 class Array
   def sandwich_in(n=nil)
-    dup_array = self.dup
-     n == nil ? acc = dup_array.shift : acc = n
-    dup_array.each { |el| acc = yield(acc, el) }
+    array_copy = self.dup
+    n == nil ? acc = array_copy.shift : acc = n
+    array_copy.each { |el| acc = yield(acc, el) }
     acc
   end
 end
