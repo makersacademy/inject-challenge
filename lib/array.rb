e7762f348c6ac38a8cf05cdae79f341932336362
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-13 19:26:08 +0100

    Added tests

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,7 @@
 class Array
 
   def my_inject(acc=nil)
+
     acc ||= self.first
     self[1..-1].each do |elem|
       acc = yield(acc,elem)
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,38 +1,50 @@
 require 'array'
 
 describe Array do
-  let(:array) { [1,2,3,4]}
 
-  describe "#my_inject with integers" do
+  let(:array) {[1,2,3,4]}
 
-    add = Proc.new { |acc, elem| acc + elem }
-    sub = Proc.new { |acc, elem| acc - elem }
-    mul = Proc.new { |acc, elem| acc * elem }
-    div = Proc.new { |acc, elem| acc / elem }
-    doub = Proc.new { |acc, elem| acc << 2*elem }
+  add = Proc.new { |acc, elem| acc + elem }
+  sub = Proc.new { |acc, elem| acc - elem }
+  mul = Proc.new { |acc, elem| acc * elem }
+  div = Proc.new { |acc, elem| acc / elem }
+  doub = Proc.new { |acc, elem| acc << 2*elem }
+  squ = Proc.new { |acc, elem| acc << elem * elem}
 
-    it { is_expected.to respond_to(:my_inject)}
+  describe "#my_inject using procs for testing integers" do
 
-    it "will act like inject with addition" do
-      expect(array.my_inject &add).to eq(10)
+      it { is_expected.to respond_to(:my_inject).with(1).argument}
+
+      it "will act like inject with addition" do
+        result = array.my_inject { |acc, elem| acc + elem }
+        expect(array.my_inject &add).to eq(result)
+      end
+
+      it "will act like inject with subraction" do
+        result = array.my_inject { |acc, elem| acc - elem }
+        expect(array.my_inject &sub).to eq(-8)
+      end
+
+      it "will act like inject with multiplication" do
+        result = array.my_inject { |acc, elem| acc * elem }
+        expect(array.my_inject &mul).to eq(result)
+      end
+
+      it "will act like inject with divsion" do
+        result = array.my_inject { |acc, elem| acc / elem }
+        expect(array.my_inject &div).to eq(result)
+      end
+
+      it "will act like inject with doubling" do
+        result = array.my_inject { |acc, elem| acc << 2*elem }
+        expect(array.my_inject &doub).to eq(result)
+      end
+
+      it "will act like inject with sqauring" do
+        result = array.my_inject { |acc, elem| acc << elem * elem }
+        expect(array.my_inject &squ).to eq(result)
+      end
+
     end
-
-    it "will act like inject with subraction" do
-      expect(array.my_inject &sub).to eq(-8)
-    end
-
-    it "will act like inject with multiplication" do
-      expect(array.my_inject &mul).to eq(24)
-    end
-
-    it "will act like inject with divsion" do
-      expect(array.my_inject &div).to eq(0)
-    end
-
-    it "will act like inject with doubling" do
-      expect(array.my_inject &doub).to eq(262144)
-    end
-
-  end
 
 end
