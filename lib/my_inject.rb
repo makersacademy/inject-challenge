5635bf6b7d20baf6c5434ba8533febeed21deaa8
Proskurina <yproskurina@yahoo.com>
2015-09-13 13:22:00 +0100

    More ammendment as requested by hound

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,7 +1,8 @@
 class MyArray < Array
 
   def my_inject(initial = self.shift, sym=nil, &block)
-    #When there is no block and only one argument, this argument is assigned to initial.
+    #When there is no block and only one argument,
+    #this argument is assigned to initial.
     #but I need it to be symbol.
     #The next 4 lines is the only way I could find to deal with it.
     #Any advise is highly appreciated
@@ -20,15 +21,17 @@ class MyArray < Array
     end
   end
 
-  def my_inject_rec(initial = self.shift, sym=nil, &block)
+  def my_inject_rec(initial=self.shift,sym=nil,&block)
     if !block && (initial.is_a? Symbol)
       sym = initial
       initial = self.shift
     end
     if block
-      size == 0 ? initial : initial = my_inject_rec(block.call(initial, self.shift), &block)
+      size == 0 ? initial :
+      initial = my_inject_rec(block.call(initial, self.shift), &block)
     elsif sym
-      size == 0 ? initial : initial = my_inject_rec(initial.send(sym, self.shift), sym)
+      size == 0 ? initial :
+      initial = my_inject_rec(initial.send(sym, self.shift), sym)
     else
       fail "ERROR!"
     end
